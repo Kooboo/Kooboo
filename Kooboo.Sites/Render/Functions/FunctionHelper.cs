9f031beab25a9fc1b39be6c91b0240ea03499809
zguoqi <guoqi@kooboo.com>
2018-10-08 16:44:59 +0800

    remove TAl function. add server bar. Add ECMAScript project.

--- Kooboo.Sites/Render/Functions/FunctionHelper.cs
+++ Kooboo.Sites/Render/Functions/FunctionHelper.cs
@@ -1,7 +1,6 @@
 //Copyright (c) 2018 Yardi Technology Limited. Http://www.kooboo.com 
 //All rights reserved.
 using Kooboo.Data.Context;
-using Kooboo.TAL;
 using System.Collections.Generic;
 
 namespace Kooboo.Sites.Render.Functions
