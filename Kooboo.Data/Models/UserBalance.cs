d4392aaf4f0058f28bf080da6febabf42abbc710
chenzhen <chenzhen@kooboo.cn>
2018-10-16 15:21:20 +0800

    kooboo.data

---
+++ Kooboo.Data/Models/UserBalance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kooboo.Data.Models
+{
+    public class UserBalance
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Balance { get; set; } = 0;
+    }
+}
