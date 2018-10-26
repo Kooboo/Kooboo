a1f2bb7e7942ae18d3187e3d70eea1edb90644d7
chenzhen <chenzhen@kooboo.cn>
2018-10-26 11:44:38 +0800

    order

---
+++ Kooboo.Data/Models/Order.cs
@@ -0,0 +1,70 @@
+﻿using System;
+ 
+using Kooboo.Data;
+using Kooboo.Data.Attributes;
+
+namespace Kooboo.Data.Models
+{
+    public class Order : IGolbalObject
+    {
+        private Guid _id;
+
+        public Guid Id
+        {
+            get
+            {
+                if (_id == default(Guid))
+                    _id = Guid.NewGuid();
+                return _id;
+            }
+            set { _id = value; }
+        }
+        public Guid OrganizationId { get; set; }
+
+        private SalesItem _item;
+
+        [SqlIngore]
+        public SalesItem Item
+        {
+            get { return _item; }
+            set
+            {
+                _saleItem = Lib.Helper.JsonHelper.Serialize(value);
+                _item = value;
+            }
+        }
+
+        private string _saleItem;
+
+        public string SaleItem
+        {
+            get { return _saleItem; }
+            set
+            {
+                _item = Lib.Helper.JsonHelper.Deserialize<SalesItem>(value);
+                _saleItem = value;
+            }
+        }
+
+        public DateTime CreationDate { get; set; } = DateTime.UtcNow;
+
+        public OrderStatus Status { get; set; }
+
+        public decimal TotalPrice
+        {
+            get;set;
+        }
+
+        public string Currency { get; set; }
+        
+    }
+
+    public enum OrderStatus
+    {
+        Unknown,
+        Unpaid,
+        Paid,
+        Canceled,
+        Delivered
+    }
+}
