71afaaee98447b62b37d68b04922c65a75944a96
chenzhen <chenzhen@kooboo.cn>
2018-10-26 16:03:34 +0800

    balance

--- Kooboo.Data/Models/UserBalance.cs
+++ Kooboo.Data/Models/UserBalance.cs
@@ -11,5 +11,6 @@ namespace Kooboo.Data.Models
         public Guid Id { get; set; }
         public string Name { get; set; }
         public decimal Balance { get; set; } = 0;
+        public Currency Currency { get; set; }
     }
 }
