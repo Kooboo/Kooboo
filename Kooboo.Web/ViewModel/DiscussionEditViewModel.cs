b442eb1da298ea726bc5d85b1e1c8e5020f91757
chenzhen <chenzhen@kooboo.cn>
2018-11-01 18:17:38 +0800

    editViewModel

---
+++ Kooboo.Web/ViewModel/DiscussionEditViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kooboo.Web.ViewModel
+{
+    public class DiscussionEditViewModel
+    {
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+    }
+}
