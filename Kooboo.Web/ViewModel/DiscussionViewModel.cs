c1314dc480e07c6f84f8f4aba38bda810e9800bb
chenzhen <chenzhen@kooboo.cn>
2018-11-01 09:38:25 +0800

    viewModel

---
+++ Kooboo.Web/ViewModel/DiscussionViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kooboo.Web.ViewModel
+{
+    public class DiscussionViewModel
+    {
+        public Guid Id { get; set; }
+        
+        public string Title { get; set; }
+
+        public string UserName { get; set; }
+
+        public DateTime LastModified { get; set; }
+
+        public int ViewCount { get; set; }
+
+        public int CommentCount { get; set; }
+    }
+}
