7069898de63c3ea73cb05e76ca82f4e447628c7f
chenzhen <chenzhen@kooboo.cn>
2018-11-14 14:25:11 +0800

    demand proposal

---
+++ Kooboo.Web/ViewModel/DemandProposalViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kooboo.Web.ViewModel
+{
+    public class DemandProposalViewModel
+    {
+        public string Description { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Budget { get; set; }
+
+        public bool WinTheBidding { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        /// <summary>
+        /// days
+        /// </summary>
+        public int Duration { get; set; }
+    }
+}
