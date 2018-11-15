3de2ce9033235d6d07fc0f23d1e82b5099e70a17
chenzhen <chenzhen@kooboo.cn>
2018-11-15 12:00:08 +0800

    proposal

--- Kooboo.Web/ViewModel/DemandProposalViewModel.cs
+++ Kooboo.Web/ViewModel/DemandProposalViewModel.cs
@@ -8,6 +8,8 @@ namespace Kooboo.Web.ViewModel
 {
     public class DemandProposalViewModel
     {
+        public Guid Id { get; set; }
+
         public string Description { get; set; }
 
         public string UserName { get; set; }
