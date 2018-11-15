4933379937017150e0d12c504d948b6f9bfae414
chenzhen <chenzhen@kooboo.cn>
2018-11-15 09:47:07 +0800

    demand add userId

--- Kooboo.Web/ViewModel/DemandItemViewModel.cs
+++ Kooboo.Web/ViewModel/DemandItemViewModel.cs
@@ -32,6 +32,8 @@ namespace Kooboo.Web.ViewModel
 
         public string Description { get; set; }
 
+        public Guid UserId { get; set; }
+
         public string UserName { get; set; }
 
         public JObject Status { get; set; }
