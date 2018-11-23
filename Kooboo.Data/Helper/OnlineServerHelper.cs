7161e267de671a69b5bf6c568895302000b2aee3
chenzhen <chenzhen@kooboo.cn>
2018-11-23 19:09:09 +0800

    permission add

---
+++ Kooboo.Data/Helper/OnlineServerHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kooboo.Data.Models;
+
+namespace Kooboo.Data.Helper
+{
+    public class OnlineServerHelper
+    {
+        private static string RootUrl = "https://162.211.126.186:50005/_api/";
+        private static List<OnlineServer> cacheServers { get; set; }
+        private static DateTime NextCheck { get; set; }
+
+        public static List<OnlineServer> AllServers()
+        {
+            if (NextCheck < DateTime.Now)
+            {
+                cacheServers = GetAllServers();
+                NextCheck = DateTime.Now.AddHours(1);
+            }
+            return cacheServers;
+        }
+
+        public static List<OnlineServer> GetAllServers()
+        {
+            try
+            {
+                string serverurl = RootUrl + "Server/all?AccessToken=6db401e9-3a9c-9ec0-04af-910b55d8a85b";
+                return Lib.Helper.HttpHelper.Get<List<OnlineServer>>(serverurl);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            List<OnlineServer> result = new List<OnlineServer>();
+            return result;
+        }
+    }
+}
---
+++ Kooboo.Data/Helper/PermissionHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kooboo.Data.Helper
+{
+    public class PermissionHelper
+    {
+        public static bool IsAllowed(string ip)
+        {
+            var allServers = OnlineServerHelper.AllServers();
+            foreach (var item in allServers)
+            {
+                if (Lib.Helper.IPHelper.IsInSameCClass(ip, item.PrimaryIp))
+                {
+                    return true;
+                }
+            }
+
+            if (Lib.Helper.IPHelper.IsLocalIp(ip))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
