fbbce7943446e4383bbaa2a95ffa032f4f835a94
zguoqi <guoqi@kooboo.com>
2019-02-17 21:28:13 +0800

    account API bug

---
+++ Kooboo.Web/Modules/TestSiteModuleApi.cs
@@ -0,0 +1,27 @@
+﻿using Kooboo.Api;
+using Kooboo.Module;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kooboo.Web.Modules
+{
+    public class TestSiteModuleApi : ISiteModuleApi
+    {
+        public string ModelName => "WebTestSample";
+
+        public Dictionary<string, string> methodone(ApiCall call)
+        {
+            Dictionary<string, string> response = new Dictionary<string, string>();
+            response.Add("callfrom", call.Context.Request.IP); 
+            if (call.WebSite !=null)
+            {
+                response.Add("sitename", call.WebSite.Name); 
+            }
+
+            return response; 
+        }
+    }
+}
