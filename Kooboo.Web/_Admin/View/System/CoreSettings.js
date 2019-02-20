f94b4764d301de9839ed197642eb7e7a25f9f284
kooboo-kaiquan <kaiquan@kooboo.cn>
2019-02-20 18:20:11 +0800

    core setting

---
+++ Kooboo.Web/_Admin/View/System/CoreSettings.js
@@ -0,0 +1,55 @@
+$(function() {
+    var CoreSetting = function() {
+        var self = this;
+
+        this.settings = ko.observableArray();
+
+        this.getList = function() {
+            Kooboo.CoreSetting.getList().then(function(res) {
+                if (res.success) {
+                    self.settings(res.model.map(function(item) {
+                        return {
+                            name: item.name,
+                            value: item.value
+                        }
+                    }))
+                }
+            })
+        }
+
+        this.showModal = ko.observable(false);
+        this.fields = ko.observableArray();
+        this.currentSettingName = ko.observable();
+        this.onSave = function() {
+            Kooboo.CoreSetting.update({
+                name: self.currentSettingName(),
+                model: Kooboo.arrToObj(self.fields(), 'name', 'value')
+            }).then(function(res) {
+                if (res.success) {
+                    info.done(Kooboo.text.info.update.success);
+                    self.onClose();
+                }
+            })
+        }
+        this.onClose = function() {
+            self.currentSettingName('');
+            self.fields([]);
+            self.showModal(false);
+        }
+
+        this.onEdit = function(name) {
+            self.currentSettingName(name);
+            Kooboo.CoreSetting.get({
+                name: name
+            }).then(function(res) {
+                self.fields(Kooboo.objToArr(res.model, 'name', 'value'))
+                self.showModal(true);
+            })
+        }
+
+        this.getList();
+    }
+
+    var vm = new CoreSetting();
+    ko.applyBindings(vm, document.getElementById('main'));
+})
