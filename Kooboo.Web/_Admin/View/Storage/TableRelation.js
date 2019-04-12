6a9ed51ccf5144292fe5159e72187d468d862634
kooboo-kaiquan <kaiquan@kooboo.cn>
2019-04-12 17:38:36 +0800

    table relation table

--- Kooboo.Web/_Admin/View/Storage/TableRelation.js
+++ Kooboo.Web/_Admin/View/Storage/TableRelation.js
@@ -13,6 +13,51 @@ $(function() {
     function getList() {
       Kooboo.TableRelation.getList().then(function(res) {
         if (res.success) {
+          self.tableData({
+            docs: res.model.map(function(item) {
+              return {
+                name: item.name,
+                tablea: item.tablea,
+                fielda: item.fielda,
+                tableb: item.tableb,
+                fieldb: item.fieldb,
+                relation: item.relation.name
+              };
+            }),
+            columns: [
+              {
+                displayName: "Name",
+                fieldName: "name",
+                type: "text"
+              },
+              {
+                displayName: "Main table",
+                fieldName: "tablea",
+                type: "text"
+              },
+              {
+                displayName: "Main table field",
+                fieldName: "fielda",
+                type: "text"
+              },
+              {
+                displayName: "Relation",
+                fieldName: "relation",
+                type: "text"
+              },
+              {
+                displayName: "Sub table",
+                fieldName: "tableb",
+                type: "text"
+              },
+              {
+                displayName: "Sub table field",
+                fieldName: "fieldb",
+                type: "text"
+              }
+            ],
+            kbType: Kooboo.TableRelation.name
+          });
         }
       });
     }
