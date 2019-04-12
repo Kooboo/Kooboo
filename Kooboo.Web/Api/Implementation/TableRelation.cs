f2fb974ad6149b536fe9a4f538977646cb436ba3
zguoqi <guoqi@kooboo.com>
2019-04-12 17:16:08 +0800

    table relation list

--- Kooboo.Web/Api/Implementation/TableRelation.cs
+++ Kooboo.Web/Api/Implementation/TableRelation.cs
@@ -10,10 +10,10 @@ using System.Text;
 namespace Kooboo.Web.Api.Implementation
 {
     public class TableRelationApi : SiteObjectApi<TableRelation>
-    { 
-        public List<TableRelationViewModel> getTablesAndFields(ApiCall call)
+    {
+        public List<TableFieldsViewModel> getTablesAndFields(ApiCall call)
         {
-            List<TableRelationViewModel> Result = new List<TableRelationViewModel>();
+            List<TableFieldsViewModel> Result = new List<TableFieldsViewModel>();
 
             var db = Kooboo.Data.DB.GetKDatabase(call.Context.WebSite);
             var tablelist = db.GetTables();
@@ -23,7 +23,7 @@ namespace Kooboo.Web.Api.Implementation
                 var table = db.GetOrCreateTable(item);
                 if (table != null)
                 {
-                    TableRelationViewModel model = new TableRelationViewModel();
+                    TableFieldsViewModel model = new TableFieldsViewModel();
 
                     model.Name = table.Name;
                     foreach (var col in table.Setting.Columns)
@@ -40,42 +40,93 @@ namespace Kooboo.Web.Api.Implementation
 
             return Result;
         }
-         
+
         public override Guid Post(ApiCall call)
         {
             return base.Post(call);
         }
 
-        public List<RelationTypeViewModel> GetRelationTypes (ApiCall call)
+        public List<RelationTypeViewModel> GetRelationTypes(ApiCall call)
+        {
+            List<RelationTypeViewModel> Result = _GetRelationTypes(call);
+
+            return Result;
+        }
+
+        private List<RelationTypeViewModel> _GetRelationTypes(ApiCall call)
         {
             List<RelationTypeViewModel> Result = new List<RelationTypeViewModel>();
 
             Result.Add(new RelationTypeViewModel() { Type = EnumTableRelation.OneOne.ToString(), DisplayName = Kooboo.Data.Language.Hardcoded.GetValue("OneOne", call.Context) });
-             
+
             Result.Add(new RelationTypeViewModel() { Type = EnumTableRelation.OneMany.ToString(), DisplayName = Kooboo.Data.Language.Hardcoded.GetValue("OneMany", call.Context) });
 
             Result.Add(new RelationTypeViewModel() { Type = EnumTableRelation.ManyMany.ToString(), DisplayName = Kooboo.Data.Language.Hardcoded.GetValue("ManyMany", call.Context) });
 
             Result.Add(new RelationTypeViewModel() { Type = EnumTableRelation.ManyOne.ToString(), DisplayName = Kooboo.Data.Language.Hardcoded.GetValue("ManyOne", call.Context) });
-             
-            return Result; 
+            return Result;
         }
 
-        public   List<TableRelation> RelationList(ApiCall call)
+        public override List<object> List(ApiCall call)
         {
-            var sitedb = call.Context.WebSite.SiteDb(); 
-            return sitedb.GetSiteRepository<TableRelationRepository>().All(); 
-        } 
+            var types = _GetRelationTypes(call);
+            var sitedb = call.Context.WebSite.SiteDb();
+            var list = sitedb.GetSiteRepository<TableRelationRepository>().All();
+
+            List<TableRelationViewModel> result = new List<TableRelationViewModel>();
+            foreach (var item in list)
+            {
+                var model = new TableRelationViewModel(item);
+                if (item.Relation == EnumTableRelation.OneOne)
+                {
+                    model.DisplayName = Data.Language.Hardcoded.GetValue("OneOne", call.Context);
+                }
+                else if (item.Relation == EnumTableRelation.OneMany)
+                {
+                    model.DisplayName = Data.Language.Hardcoded.GetValue("OneMany", call.Context);
+                }
+                else if (item.Relation == EnumTableRelation.ManyMany)
+                {
+                    model.DisplayName = Data.Language.Hardcoded.GetValue("ManyMany", call.Context);
+                }
+                else if (item.Relation == EnumTableRelation.ManyOne)
+                {
+                    model.DisplayName = Data.Language.Hardcoded.GetValue("ManyOne", call.Context);
+                }
+
+                result.Add(model);
+
+            }
+
+            return result.ToList<object>();
+        }
     }
 
-    public class  RelationTypeViewModel
+    public class TableRelationViewModel : TableRelation
+    {
+        public TableRelationViewModel(TableRelation relation)
+        {
+            this.FieldA = relation.FieldA;
+            this.FieldB = relation.FieldB;
+            this.Relation = relation.Relation;
+            this.TableA = relation.TableA;
+            this.TableB = relation.TableB;
+        }
+
+        public string DisplayName { get; set; }
+
+    }
+
+
+
+    public class RelationTypeViewModel
     {
         public string Type { get; set; }
 
         public string DisplayName { get; set; }
     }
 
-    public class TableRelationViewModel
+    public class TableFieldsViewModel
     {
         public string Name { get; set; }
 
