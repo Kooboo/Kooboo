1a2611b303d9d410efad7a51083b4371301a295a
zguoqi <guoqi@kooboo.com>
2019-05-21 16:21:07 +0800

    json convert for DynamicTableObject

--- Kooboo.Sites/Scripting/Global/Response.cs
+++ Kooboo.Sites/Scripting/Global/Response.cs
@@ -41,30 +41,30 @@ namespace Kooboo.Sites.Scripting.Global
             string output;
             if (!(value is string) && value.GetType().IsClass)
             {
-                if (value is Kooboo.Data.Interface.IDynamic)
-                {
-                    var dynamic = value as Kooboo.Data.Interface.IDynamic;
-                    output = Lib.Helper.JsonHelper.SerializeCaseSensitive(dynamic.Values);
-                }
-                else if (IsDynamicArray(value))
-                {
-                    var dynamicValues = value as ICollection;
+                //if (value is Kooboo.Data.Interface.IDynamic)
+                //{
+                //    var dynamic = value as Kooboo.Data.Interface.IDynamic;
+                //    output = Lib.Helper.JsonHelper.SerializeCaseSensitive(dynamic.Values);
+                //}
+                //else if (IsDynamicArray(value))
+                //{
+                //    var dynamicValues = value as ICollection;
 
-                    List<Dictionary<string, object>> result = new List<Dictionary<string, object>>();
-                    foreach (var v in dynamicValues)
-                    {
-                        var dynamicvalue = v as Kooboo.Data.Interface.IDynamic;
-                        if (dynamicvalue != null)
-                        {
-                            result.Add(dynamicvalue.Values);
-                        }
-                    }
-                    output = Lib.Helper.JsonHelper.SerializeCaseSensitive(result);
-                }
-                else
-                {
+                //    List<Dictionary<string, object>> result = new List<Dictionary<string, object>>();
+                //    foreach (var v in dynamicValues)
+                //    {
+                //        var dynamicvalue = v as Kooboo.Data.Interface.IDynamic;
+                //        if (dynamicvalue != null)
+                //        {
+                //            result.Add(dynamicvalue.Values);
+                //        }
+                //    }
+                //    output = Lib.Helper.JsonHelper.SerializeCaseSensitive(result);
+                //}
+                //else
+                //{
                     output = Lib.Helper.JsonHelper.SerializeCaseSensitive(value);
-                }
+                //}
             }
             else
             {
@@ -89,48 +89,6 @@ namespace Kooboo.Sites.Scripting.Global
             write(value);
         }
 
-        public bool IsDynamicArray(object value)
-        {
-            var type = value.GetType();
-
-            if (Lib.Reflection.TypeHelper.IsCollection(type))
-            {
-                if (type.IsGenericType)
-                {
-                    if (Lib.Reflection.TypeHelper.GetGenericType(value.GetType()) == typeof(Kooboo.Data.Interface.IDynamic))
-                    {
-                        return true;
-                    }
-                    else
-                    {
-                        return false; 
-                    } 
-                }
-                else
-                {
-                    var oneitem = GetOneItem(value as ICollection);
-                    var IDynamicItem = oneitem as Kooboo.Data.Interface.IDynamic;
-
-                    return IDynamicItem != null;  
-                } 
-
-            }
-
-            return false; 
-        }
-
-        public object GetOneItem(ICollection col)
-        {
-            if (col == null)
-            {
-                return null; 
-            }
-            foreach (var item in col)
-            {
-                return item; 
-            }
-            return null; 
-        }
-
+       
     }
 }
