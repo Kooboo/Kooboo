e39ed06f669bd0bcebfd9101c72498fef7f6ae9d
JongWong <JongWong@DESKTOP-EUK9SQS>
2019-07-10 12:37:23 +0800

    shade block test

---
+++ Kooboo.Web/kooboo-web-editor/tests/components/editorShade/block.test.ts
@@ -0,0 +1,14 @@
+import { createBlock } from "@/components/editorShade/block";
+import { EDITOR_SHADE_COLOR, STANDARD_Z_INDEX } from "@/common/constants";
+
+test("create shade block", () => {
+  let block = createBlock();
+  block.update({ top: 1, left: 2, width: 3, height: 4 });
+  expect(block.el.style.position).toEqual("absolute");
+  expect(block.el.style.display).toEqual("block");
+  let backgroundColor = block.el.style.backgroundColor;
+  // @ts-ignore
+  expect(backgroundColor.replace(/\s/g, "")).toEqual(EDITOR_SHADE_COLOR.replace(/\s/g, ""));
+  expect(block.el.style.zIndex).toEqual(STANDARD_Z_INDEX + "");
+  expect(block.el.style.cursor).toEqual("not-allowed");
+});
