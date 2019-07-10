b1f8420e379e67830ed00ba2b5839fa1c0895443
李凤焕 <huanent@hotmail.com>
2019-07-10 18:25:31 +0800

    add stop link element click

--- Kooboo.Web/kooboo-web-editor/src/common/utils.ts
+++ Kooboo.Web/kooboo-web-editor/src/common/utils.ts
@@ -1,5 +1,16 @@
+import { getAllElement, isLink } from "@/dom/utils";
+
 export function delay(time: number) {
   return new Promise(rs => {
     setTimeout(rs, time);
   });
+}
+
+export function stopLinkElementClick() {
+  for (const iterator of getAllElement(document.body)) {
+    if (iterator instanceof HTMLElement && isLink(iterator)) {
+      let a = iterator.cloneNode(true);
+      iterator.parentElement!.replaceChild(a, iterator);
+    }
+  }
 }
--- Kooboo.Web/kooboo-web-editor/src/index.ts
+++ Kooboo.Web/kooboo-web-editor/src/index.ts
@@ -4,9 +4,11 @@ import { createHoverBorder } from "./components/selectorBorder";
 import { createContainer } from "./components/container";
 import { createEditorShade } from "./components/editorShade";
 import { listenHover, listenClick } from "./dom/events";
+import { stopLinkElementClick } from "./common/utils";
 
 listenHover();
 listenClick();
+stopLinkElementClick();
 
 const container = createContainer();
 container.appendChild(createHoverBorder());
