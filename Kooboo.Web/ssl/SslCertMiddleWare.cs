12e7f66fb3f35c4aaa9ef8891666e5766e28e190
zguoqi <guoqi@kooboo.com>
2019-07-31 16:41:11 +0800

    ssl certificate generator on web.

---
+++ Kooboo.Web/ssl/SslCertMiddleWare.cs
@@ -0,0 +1,75 @@
+﻿using Kooboo.Data.Context;
+using Kooboo.Data.Server;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Kooboo.ServerData.Ssl
+{
+    public class SslCertMiddleWare : IKoobooMiddleWare
+    {
+        public IKoobooMiddleWare Next
+        {
+            get; set;
+        }
+        public async Task Invoke(RenderContext context)
+        {
+            /// http://your.domain.name/.well-known/acme-challenge/
+            if (context.Request.RelativeUrl != null && context.Request.RelativeUrl.ToLower().StartsWith("/.well-known/acme-challenge"))
+            {
+                var host = context.Request.Host;
+
+                //var ssltoke = SslTokenService.GetUseDomain(host);
+                //if (!string.IsNullOrEmpty(ssltoke))
+                //{
+                //    context.Response.Body = System.Text.Encoding.UTF8.GetBytes(ssltoke);
+                //    context.Response.ContentType = "text/html;charset=utf-8; ";
+                //    context.Response.End = true;
+                //    return;
+                //}
+
+                string validate = context.Request.QueryString.Get("validate");
+
+                if (string.IsNullOrWhiteSpace(validate))
+                {
+                    // this is for real... not for validation check. 
+                    string token = Kooboo.Web.ssl.Sslvalidation.GetToken(host);
+                    if (!string.IsNullOrWhiteSpace(token))
+                    { 
+                        context.Response.Body = System.Text.Encoding.UTF8.GetBytes(token);
+                        context.Response.ContentType = "text/html;charset=utf-8; ";
+                        context.Response.End = true;
+                    }
+                }
+                else
+                {
+                    var checkok = Kooboo.Web.ssl.Sslvalidation.Verify(host, validate);
+
+                    if (checkok)
+                    {
+                        context.Response.Body = System.Text.Encoding.UTF8.GetBytes("[OK]");
+                        context.Response.ContentType = "text/html;charset=utf-8; ";
+                        context.Response.End = true;
+                    }
+                    else
+                    {
+                        context.Response.Body = System.Text.Encoding.UTF8.GetBytes("[error]");
+                        context.Response.StatusCode = 403;
+                        context.Response.ContentType = "text/html;charset=utf-8; ";
+                        context.Response.End = true;
+                    }
+                }
+
+            }
+            else
+            {
+                await Next.Invoke(context);
+            }
+        }
+
+
+   
+    }
+}
+
+
+
---
+++ Kooboo.Web/ssl/SslValidation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kooboo.Web.ssl
+{
+    
+    public static  class Sslvalidation
+    {
+        static Sslvalidation()
+        {
+            tokenurl ="http://www.sslgenerator.com";
+        }
+
+        public static string tokenurl { get; set; }
+         
+        public static bool Verify(string domain, string code)
+        {
+            Dictionary<string, string> para = new Dictionary<string, string>();
+            para.Add("domain", domain);
+            para.Add("code", code);
+            string vefiryurl = tokenurl + "/_api/validator/SelfCheck";
+            return Lib.Helper.HttpHelper.TryGet<bool>(vefiryurl, para);
+        }
+
+
+        public static string GetToken(string domain)
+        {
+            Dictionary<string, string> para = new Dictionary<string, string>();
+            para.Add("domain", domain);
+            var  url  = tokenurl + "/_api/validator/HttpToken";
+            return Lib.Helper.HttpHelper.TryGet<string>(url, para);
+        }
+          
+    }
+
+
+}
