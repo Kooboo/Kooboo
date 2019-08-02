841d43853bec976582b346810c04576ae38d87a4
jongwong <32861357+jongwong@users.noreply.github.com>
2019-08-02 14:58:18 +0800

    Background Class test

---
+++ Kooboo.Web/kooboo-web-editor/tests/dom/Background.test.ts
@@ -0,0 +1,13 @@
+import { Background } from "@/dom/Background";
+
+describe("Background class test", () => {
+  test("Background constructor", () => {
+    let el = document.createElement("div") as HTMLElement;
+    el.style.background = "rgb(255, 255, 255) url(test.png) no-repeat fixed top";
+    let background = new Background(el.style.background);
+    expect(background.image).toEqual("url(test.png)");
+    expect(background.color).toEqual("rgb(255, 255, 255)");
+    expect(background.repeat).toEqual("no-repeat");
+    expect(background.position).toEqual("top");
+  });
+});
