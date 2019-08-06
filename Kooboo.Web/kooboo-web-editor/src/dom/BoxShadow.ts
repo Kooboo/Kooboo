8fd8b997207e76d99c059fd9898dfe2bde850ce3
qiaolong <1373611035@qq.com>
2019-08-06 17:31:27 +0800

    添加解析

---
+++ Kooboo.Web/kooboo-web-editor/src/dom/BoxShadow.ts
@@ -0,0 +1,52 @@
+export class BoxShadow{
+    constructor(public value: string){
+        // let temp = document.createElement("div");
+        // temp.style.boxShadow = value;
+
+        value = value.replace(";", "");
+        if(value.indexOf("inset") > -1){
+            value = value.replace("inset", "");
+            this.inset = "inset";
+        }
+
+        value = value.replace(/\s+/, " ");
+        let values = value.trim().split(" ");
+        if(values.length < 3 || values.length > 5){
+            return;
+        }
+
+        if(values.length == 3){
+            this.hShadow = values[0];
+            this.vShadow = values[1];
+            this.color = values[2];
+        }
+
+        if(values.length == 4){
+            this.hShadow = values[0];
+            this.vShadow = values[1];
+            this.blur = values[2];
+            this.color = values[3];
+        }
+
+        if(values.length == 5){
+            this.hShadow = values[0];
+            this.vShadow = values[1];
+            this.blur = values[2];
+            this.spread = values[3];
+            this.color = values[4];
+        }
+    }
+
+    hShadow: string = "0px";
+    vShadow: string = "0px";
+    blur: string = "0px";
+    spread: string = "0px";
+    color: string = "#fff0";
+    inset: string | null = null;
+
+    toString(){
+        let result = `${this.hShadow} ${this.vShadow} ${this.blur} ${this.spread} ${this.color} ${this.inset || ""}`;
+
+        return result;
+    }
+}
---
+++ Kooboo.Web/kooboo-web-editor/src/dom/Outline.ts
@@ -0,0 +1,28 @@
+export class Outline {
+    constructor(public value: string) {
+        let temp = document.createElement("div");
+        temp.style.border = value;
+
+        this.width = temp.style.borderWidth!;
+        this.style = temp.style.borderStyle!;
+        this.color = temp.style.borderColor!;
+    }
+
+    color: string;
+    style: string;
+    width: string;
+
+    toString() {
+        let result = "";
+        if (this.width && this.width != "initial")
+            result += `${this.width} `;
+
+        if (this.style && this.style != "initial")
+            result += `${this.style} `;
+
+        if (this.color && this.color != "initial")
+            result += `${this.color} `;
+
+        return result;
+    }
+}
