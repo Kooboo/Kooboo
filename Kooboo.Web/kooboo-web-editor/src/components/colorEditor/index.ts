cac9b6300442e637a55aa6a5aafff5986f2df506
qiaolong <1373611035@qq.com>
2019-08-09 16:49:47 +0800

    重构createColorEditor

--- Kooboo.Web/kooboo-web-editor/src/components/colorEditor/index.ts
+++ Kooboo.Web/kooboo-web-editor/src/components/colorEditor/index.ts
@@ -1,6 +1,6 @@
 import { createModal } from "../modal";
 import { TEXT } from "@/common/lang";
-import { getMatchedColorGroups, CssColorGroup } from "@/dom/style";
+import { getMatchedColorGroups, CssColorGroup, CssColor } from "@/dom/style";
 import { parentBody } from "@/kooboo/outsideInterfaces";
 import { createDiv, createCheckboxInput } from "@/dom/element";
 import { createColorPicker } from "../common/colorPicker";
@@ -48,73 +48,90 @@ export function createColorEditor(el: HTMLElement, comment: KoobooComment, koobo
 function createItem(colorGroup: CssColorGroup, el: HTMLElement, comment: KoobooComment, koobooId: string) {
   let container = createDiv();
   container.style.display = "flex";
-  let inlineLog: StyleLog | undefined;
-  let cssLog: StyleLog | undefined;
-  let color = colorGroup.cssColors[0];
-  let globalUpdate = !color.inline;
-  let inlineColor = colorGroup.cssColors.find(f => f.inline);
-  let cssColor = colorGroup.cssColors.find(f => !f.inline);
-  let updateColor = inlineColor || color;
-  let label = colorGroup.prop + colorGroup.pseudo;
-  let oldValue = color.prop.getColor(color.value);
-  let picker = createColorPicker(label, oldValue, s => {
-    if (globalUpdate) {
-      if (!cssColor) return;
-      let important = cssColor.cssStyleRule!.style.getPropertyPriority(cssColor.prop.prop);
-      let value = cssColor.prop.replaceColor(cssColor.value, s);
-      cssColor.cssStyleRule!.style.setProperty(cssColor.prop.prop, value, important);
-      cssColor.newValue = value;
-      cssColor.newImportant = important;
-      cssLog = StyleLog.createCssUpdate(value, cssColor.prop.prop, cssColor.rawSelector, cssColor.koobooId, cssColor.url!, !!important);
+  container.style.alignItems = "center";
+  container.style.height = "38px";
+
+  let colorTextSpan = document.createElement("span");
+  colorTextSpan.style.width = "160px";
+
+  // 内联日志
+  let inlineCssLog: StyleLog | undefined;
+  // 类日志
+  let classCssLog: StyleLog | undefined;
+
+  // 最高优先级cssColor
+  let firstCssColor = colorGroup.cssColors[0];
+  // 是否全局更新
+  let isGlobalUpdate = !firstCssColor.inline;
+  // 更新使用的内联样式
+  let inlineCssColor = colorGroup.cssColors.find(f => f.inline);
+  // 更新使用的类样式
+  let classCssColor = colorGroup.cssColors.find(f => !f.inline);
+
+  let updateColor = inlineCssColor || firstCssColor;
+  colorTextSpan.innerText = firstCssColor.prop.getColor(firstCssColor.value);
+  let picker = createColorPicker(
+    colorGroup.prop + colorGroup.pseudo, 
+    firstCssColor.prop.getColor(firstCssColor.value), 
+    s => {
+    colorTextSpan.innerText = toInt(s);
+    if (isGlobalUpdate) {
+      if (!classCssColor) return;
+      
+      let value = updateClassCss(classCssColor, s);
+      classCssLog = StyleLog.createCssUpdate(value, classCssColor.prop.prop, classCssColor.rawSelector, classCssColor.koobooId, classCssColor.url!, !!classCssColor.newImportant);
     } else {
-      let value = el.style.getPropertyValue(updateColor.prop.prop);
-      let important = el.style.getPropertyPriority(updateColor.prop.prop);
-      value = updateColor.prop.replaceColor(value, s);
-      el.style.setProperty(updateColor.prop.prop, value, important);
-      inlineLog = StyleLog.createUpdate(comment.nameorid!, comment.objecttype!, value, updateColor.prop.prop, koobooId);
+      let value = updateInlineCss(updateColor, s, el);
+      inlineCssLog = StyleLog.createUpdate(comment.nameorid!, comment.objecttype!, value, updateColor.prop.prop, koobooId);
     }
   });
   picker.style.width = "200px";
+  picker.style.justifyContent = "flex-end";
   container.appendChild(picker);
 
+  container.appendChild(colorTextSpan);
+
   const onChecked = (checked: boolean) => {
-    if (!cssColor) return;
+    if (!classCssColor) return;
+
     if (checked) {
       let value = el.style.getPropertyValue(updateColor.prop.prop);
       value = updateColor.prop.getColor(value);
-      let important = el.style.getPropertyPriority(updateColor.prop.prop);
-      value = cssColor.prop.replaceColor(cssColor.value, value);
-      cssColor.cssStyleRule!.style.setProperty(cssColor.prop.prop, value, important);
-      cssColor.newValue = value;
-      cssColor.newImportant = important;
+      
+      updateClassCss(classCssColor, value);
+
       el.style.removeProperty(updateColor.prop.prop);
-      inlineLog = StyleLog.createUpdate(comment.nameorid!, comment.objecttype!, "", updateColor.prop.prop, koobooId);
-      cssLog = StyleLog.createCssUpdate(value, cssColor.prop.prop, cssColor.rawSelector, cssColor.koobooId, cssColor.url!, !!important);
-      globalUpdate = true;
+      inlineCssLog = StyleLog.createUpdate(comment.nameorid!, comment.objecttype!, "", updateColor.prop.prop, koobooId);
+      classCssLog = StyleLog.createCssUpdate(value, classCssColor.prop.prop, classCssColor.rawSelector, classCssColor.koobooId, classCssColor.url!, !!classCssColor.newImportant);
+      isGlobalUpdate = true;
     } else {
-      let important = cssColor.cssStyleRule!.style.getPropertyPriority(cssColor.prop.prop);
-      let value = cssColor.cssStyleRule!.style.getPropertyValue(cssColor.prop.prop);
-      value = cssColor.prop.getColor(value);
+      let important = classCssColor.cssStyleRule!.style.getPropertyPriority(classCssColor.prop.prop);
+      let value = classCssColor.cssStyleRule!.style.getPropertyValue(classCssColor.prop.prop);
+
+      value = classCssColor.prop.getColor(value);
       value = updateColor.prop.replaceColor(updateColor.value, value);
       el.style.setProperty(updateColor.prop.prop, value, important);
-      cssColor.cssStyleRule!.style.setProperty(cssColor.prop.prop, cssColor.value, cssColor.important ? "important" : "");
-      cssColor.newValue = undefined;
-      cssColor.newImportant = undefined;
-      cssLog = undefined;
-      inlineLog = StyleLog.createUpdate(comment.nameorid!, comment.objecttype!, value, updateColor.prop.prop, koobooId);
-      globalUpdate = false;
+      
+      classCssColor.cssStyleRule!.style.setProperty(classCssColor.prop.prop, classCssColor.value, classCssColor.important ? "important" : "");
+      classCssColor.newValue = undefined;
+      classCssColor.newImportant = undefined;
+
+      classCssLog = undefined;
+      inlineCssLog = StyleLog.createUpdate(comment.nameorid!, comment.objecttype!, value, updateColor.prop.prop, koobooId);
+      isGlobalUpdate = false;
     }
   };
-  let { checkbox } = createCheckboxInput(TEXT.GLOBAL_UPDATE, !color.inline, onChecked);
-  if (!cssColor || colorGroup.pseudo) {
+
+  let { checkbox } = createCheckboxInput(TEXT.GLOBAL_UPDATE, !firstCssColor.inline, onChecked);
+  if (!classCssColor || colorGroup.pseudo) {
     checkbox.style.display = "none";
   }
   container.appendChild(checkbox);
 
   const getLogs = () => {
     let logs: Log[] = [];
-    if (inlineLog) logs.push(inlineLog);
-    if (cssLog) logs.push(cssLog);
+    if (inlineCssLog) logs.push(inlineCssLog);
+    if (classCssLog) logs.push(classCssLog);
     return logs;
   };
 
@@ -123,3 +140,28 @@ function createItem(colorGroup: CssColorGroup, el: HTMLElement, comment: KoobooC
     getLogs
   };
 }
+
+function updateClassCss(classCssColor: CssColor, color: string){
+  let important = classCssColor.cssStyleRule!.style.getPropertyPriority(classCssColor.prop.prop);
+      let value = classCssColor.prop.replaceColor(classCssColor.value, color);
+
+      classCssColor.cssStyleRule!.style.setProperty(classCssColor.prop.prop, value, important);
+      classCssColor.newValue = value;
+      classCssColor.newImportant = important;
+
+      return value;
+}
+
+function updateInlineCss(inlineCssColor: CssColor, color: string, el: HTMLElement){
+  let value = el.style.getPropertyValue(inlineCssColor.prop.prop);
+      let important = el.style.getPropertyPriority(inlineCssColor.prop.prop);
+
+      value = inlineCssColor.prop.replaceColor(value, color);
+      el.style.setProperty(inlineCssColor.prop.prop, value, important);
+
+      return value;
+}
+
+function toInt(color: string) {
+  return color.replace(/\..*?,/g, ",")
+};
