f85d24234a59cda5ca4cd99fe13295ead10a22db
李凤焕 <huanent@hotmail.com>
2019-08-12 17:54:10 +0800

    fix bug

--- Kooboo.Web/kooboo-web-editor/src/kooboo/utils.ts
+++ Kooboo.Web/kooboo-web-editor/src/kooboo/utils.ts
@@ -86,7 +86,6 @@ export function getGuidComment(guid: string) {
 export function isDynamicContent(el: HTMLElement) {
   for (const k in OBJECT_TYPE) {
     if (k == OBJECT_TYPE.Url) continue;
-    if (k == OBJECT_TYPE.form) continue;
     if (OBJECT_TYPE.hasOwnProperty(k)) {
       const i = OBJECT_TYPE[k as keyof typeof OBJECT_TYPE];
       if (el.innerHTML.indexOf(`objecttype='${i}'`) > -1) return true;
