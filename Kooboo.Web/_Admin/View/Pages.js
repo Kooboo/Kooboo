57fd4a77b75f550b32d978b3951785e65babd979
李凤焕 <huanent@hotmail.com>
2019-08-13 18:41:57 +0800

    fix not new inline editor button when copy page

--- Kooboo.Web/_Admin/View/Pages.js
+++ Kooboo.Web/_Admin/View/Pages.js
@@ -397,6 +397,12 @@ $(function() {
                                 url: page.inlineUrl,
                                 newWindow: true,
                                 class: "btn-primary"
+                            },
+                            design2: {
+                                text: Kooboo.text.site.page.design+"(new)",
+                                url: page.inlineUrl+"&new=true",
+                                newWindow: true,
+                                class: "btn-primary hidden-xs"
                             }
                         })
 
