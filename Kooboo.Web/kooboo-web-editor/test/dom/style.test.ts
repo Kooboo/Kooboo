9bc445009b9cda2959231f31f8394f24eb0aa379
JongWong <jongwong@aliyun.com>
2019-08-27 18:56:38 +0800

    #add test# fix style.ts && add test

--- Kooboo.Web/kooboo-web-editor/test/dom/style.test.ts
+++ Kooboo.Web/kooboo-web-editor/test/dom/style.test.ts
@@ -9,6 +9,7 @@ import {
   getMatchedColors,
   getRules,
   getStyles,
+  isOneColor,
   matchSelector,
   splitPseudo
 } from "@/dom/style";
@@ -36,7 +37,7 @@ describe("style", () => {
     }
 </style>
 <style>
-@media screen and (max-width: 300px) {
+@media screen and (max-width: 1200px) {
     #test4{
         background-color: green;
     }
@@ -50,43 +51,28 @@ describe("style", () => {
     </div>
 </div>`)
   );
-  // it("getStyles", () => {
-  //   let styles = getStyles();
-  //   let s1 = styles.next().value as CSSStyleSheet;
-  //   expect((s1.cssRules[0] as CSSStyleRule).selectorText).equal("#test1");
-  //   expect((s1.cssRules[0] as CSSStyleRule).style.getPropertyValue("background-color")).equal("white");
-  // });
-  // it("getRules", () => {
-  //   let styles = getStyles();
-  //   let s1 = styles.next().value as CSSStyleSheet;
-  //   let rules = getRules(s1);
-  //   let rule = rules.next().value.cssRule as CSSStyleRule;
-  //   expect(rule.selectorText).equal("#test1");
-  // });
-  // it("getRules_with CSSMedia", () => {
-  //   //jest不支持matchMedia，返回假数据
-  //   window.matchMedia = jest.fn().mockImplementation(query => {
-  //     return {
-  //       matches: true,
-  //       media: query,
-  //       onchange: null,
-  //       addListener: jest.fn(),
-  //       removeListener: jest.fn(),
-  //       addEventListener: jest.fn(),
-  //       removeEventListener: jest.fn(),
-  //       dispatchEvent: jest.fn()
-  //     };
-  //   });
-  //   let styles = getStyles();
-  //   styles.next();
-  //   let s1 = styles.next().value as CSSStyleSheet;
-  //   let rules = getRules(s1);
-  //   let ruleResult = rules.next();
-  //   let mediaRule = ruleResult.value.mediaRuleList;
-  //   let rule = ruleResult.value.cssRule as CSSStyleRule;
-  //   expect(mediaRule).equal("screen and (max-width: 300px)");
-  //   expect(rule.selectorText).equal("#test4");
-  // });
+  it("getStyles", () => {
+    let styles = getStyles();
+    let s1 = styles[0] as CSSStyleSheet;
+    expect((s1.cssRules[0] as CSSStyleRule).selectorText).equal("#test1");
+    expect((s1.cssRules[0] as CSSStyleRule).style.getPropertyValue("background-color")).equal("white");
+  });
+  it("getRules", () => {
+    let styles = getStyles();
+    let s1 = styles[0] as CSSStyleSheet;
+    let rules = getRules(s1);
+    let rule = rules[0].cssRule as CSSStyleRule;
+    expect(rule.selectorText).equal("#test1");
+  });
+  it("getRules_with CSSMedia", () => {
+    let styles = getStyles();
+    let s1 = styles[1] as CSSStyleSheet;
+    let rules = getRules(s1);
+    let selectorText = rules[0].cssRule.selectorText;
+    let mediaRule = rules[0].mediaRuleList;
+    expect(selectorText).equal("#test4");
+    expect(mediaRule).equal("screen and (max-width: 1200px)");
+  });
   it("getCssRules", () => {
     //待定
   });
@@ -114,10 +100,20 @@ describe("style", () => {
   });
   //getCssRules不够完整，没法测
   it("getMatchedColorGroups", () => {});
-  //理解不了（有的时候color是 #fff #fff 多个颜色组成）
-  it("isOneColor", () => {
-    let el = document.getElementById("test4") as HTMLElement;
-    let groups = getMatchedColorGroups(el);
+  it("isOneColor_not oneColor", () => {
+    let color = "red blue";
+    let isOneColorResult = isOneColor(color);
+    expect(isOneColorResult).equal(false);
+  });
+  it("isOneColor_oneColor_color", () => {
+    let color = "red";
+    let isOneColorResult = isOneColor(color);
+    expect(isOneColorResult).equal(true);
+  });
+  it("isOneColor_oneColor_colorEnum", () => {
+    let color = "aquamarine";
+    let isOneColorResult = isOneColor(color);
+    expect(isOneColorResult).equal(true);
   });
   it("splitPseudo", () => {
     let selector = "#test3:hover";
@@ -132,7 +128,6 @@ describe("style", () => {
     expect(matchSelectorResult![0].pseudo).equal(":hover");
     expect(matchSelectorResult![0].selector).equal("#test3:hover");
   });
-  //没写完
   it("addDefaultColor", () => {
     let groups: Array<CssColorGroup> = [];
     let prop = "color";
