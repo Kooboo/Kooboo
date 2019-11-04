46dd9f56c7e7a6e035a00e418416fb44a654e584
李凤焕 <huanent@hotmail.com>
2019-11-04 14:40:10 +0800

    add tooltip

--- Kooboo.Web/_Admin/Scripts/vueExtends.js
+++ Kooboo.Web/_Admin/Scripts/vueExtends.js
@@ -1,166 +1,182 @@
 Vue.prototype.Kooboo = Kooboo;
 
-// kb-tooltip
-(function() {
-  Vue.directive("kb-tooltip", {});
-})();
+// #region <kb-tooltip>
+Vue.directive("kb-tooltip", function(el, binding) {
+  let trigger = [];
+  if (binding.modifiers.focus) trigger.push("focus");
+  if (binding.modifiers.hover) trigger.push("hover");
+  if (binding.modifiers.click) trigger.push("click");
+  if (binding.modifiers.manual) trigger.push("manual");
+  trigger = trigger.join(" ");
+
+  $(el).tooltip({
+    title: binding.value,
+    placement: binding.arg,
+    trigger: trigger || "hover",
+    html: binding.modifiers.html,
+    template: binding.modifiers.error
+      ? `<div class="tooltip error" role="tooltip" style="z-index:199999">
+              <div class="tooltip-arrow"></div>
+              <div class="tooltip-inner"></div>
+            </div>`
+      : undefined,
+    container: "body"
+  });
+});
+// #endregion </kb-tooltip>
 
 //#region <kb-upload>
-$(function() {
-  Vue.directive("kb-upload", {
-    bind: function(element, binding) {
-      var config = binding.value;
-      config.allowMultiple
-        ? $(element).attr("multiple", true)
-        : $(element).removeAttr("multiple");
+Vue.directive("kb-upload", {
+  bind: function(element, binding) {
+    var config = binding.value;
+    config.allowMultiple
+      ? $(element).attr("multiple", true)
+      : $(element).removeAttr("multiple");
 
-      if (config.acceptTypes && config.acceptTypes.length) {
-        $(element).attr("accept", config.acceptTypes.join(","));
-      }
-
-      $(element).change(function() {
-        var files = this.files,
-          len = files.length,
-          acceptableFilesLength = 0;
-
-        var availableFiles = [];
-
-        if (len) {
-          var data = new FormData();
-
-          var errors = {
-            size: [],
-            type: [],
-            suffix: []
-          };
-
-          _.forEach(files, function(file, idx) {
-            var fileName = file.name;
-
-            if (!config.acceptSuffix || !config.acceptSuffix.length) {
-              if (!config.acceptTypes || !config.acceptTypes.length) {
-                alert("Upload failed: please init the acceptType first.");
-              } else {
-                if (
-                  config.acceptTypes.indexOf(file.type) > -1 ||
-                  config.acceptTypes.indexOf("*/*") > -1
-                ) {
-                  if (file.size) {
-                    data.append("file_" + idx, file);
-                    availableFiles.push(file);
-                    acceptableFilesLength++;
-                  } else {
-                    errors.size.push(file.name);
-                  }
-                } else {
-                  errors.type.push(file.name);
-                }
-              }
-            } else {
-              if (fileName.indexOf(".") > -1) {
-                var suffix = fileName
-                  .split(".")
-                  .reverse()[0]
-                  .toLowerCase();
-
-                if (config.acceptSuffix.indexOf(suffix) > -1) {
-                  if (file.size) {
-                    data.append("file_" + idx, file);
-                    availableFiles.push(file);
-                    acceptableFilesLength++;
-                  } else {
-                    errors.size.push(file.name);
-                  }
-                } else {
-                  errors.suffix.push(file.name);
-                }
-              }
-            }
-          });
-
-          config.callback(data, availableFiles);
-          resetValue(element);
-
-          var errorString = getErrorString();
-          errorString && alert(errorString);
-
-          function getErrorString() {
-            var string = "";
-            if (errors.size.length) {
-              string +=
-                Kooboo.text.common.File +
-                " " +
-                errors.size.join(", ") +
-                " " +
-                Kooboo.text.alert.fileUpload.emptyFile +
-                "\n";
-            }
-            if (errors.type.length) {
-              string +=
-                Kooboo.text.common.File +
-                " " +
-                errors.type.join(", ") +
-                " " +
-                Kooboo.text.alert.fileUpload.invalidSuffix +
-                "\n";
-            }
-            if (errors.suffix.length) {
-              string +=
-                Kooboo.text.common.File +
-                " " +
-                errors.suffix.join(", ") +
-                " " +
-                Kooboo.text.alert.fileUpload.invalidType +
-                "\n";
-            }
-            return string;
-          }
-        }
-      });
-
-      function resetValue(el) {
-        $(el)
-          .wrap("<form>")
-          .parent("form")
-          .trigger("reset");
-        $(el).unwrap();
-      }
+    if (config.acceptTypes && config.acceptTypes.length) {
+      $(element).attr("accept", config.acceptTypes.join(","));
     }
-  });
+
+    $(element).change(function() {
+      var files = this.files,
+        len = files.length,
+        acceptableFilesLength = 0;
+
+      var availableFiles = [];
+
+      if (len) {
+        var data = new FormData();
+
+        var errors = {
+          size: [],
+          type: [],
+          suffix: []
+        };
+
+        _.forEach(files, function(file, idx) {
+          var fileName = file.name;
+
+          if (!config.acceptSuffix || !config.acceptSuffix.length) {
+            if (!config.acceptTypes || !config.acceptTypes.length) {
+              alert("Upload failed: please init the acceptType first.");
+            } else {
+              if (
+                config.acceptTypes.indexOf(file.type) > -1 ||
+                config.acceptTypes.indexOf("*/*") > -1
+              ) {
+                if (file.size) {
+                  data.append("file_" + idx, file);
+                  availableFiles.push(file);
+                  acceptableFilesLength++;
+                } else {
+                  errors.size.push(file.name);
+                }
+              } else {
+                errors.type.push(file.name);
+              }
+            }
+          } else {
+            if (fileName.indexOf(".") > -1) {
+              var suffix = fileName
+                .split(".")
+                .reverse()[0]
+                .toLowerCase();
+
+              if (config.acceptSuffix.indexOf(suffix) > -1) {
+                if (file.size) {
+                  data.append("file_" + idx, file);
+                  availableFiles.push(file);
+                  acceptableFilesLength++;
+                } else {
+                  errors.size.push(file.name);
+                }
+              } else {
+                errors.suffix.push(file.name);
+              }
+            }
+          }
+        });
+
+        config.callback(data, availableFiles);
+        resetValue(element);
+
+        var errorString = getErrorString();
+        errorString && alert(errorString);
+
+        function getErrorString() {
+          var string = "";
+          if (errors.size.length) {
+            string +=
+              Kooboo.text.common.File +
+              " " +
+              errors.size.join(", ") +
+              " " +
+              Kooboo.text.alert.fileUpload.emptyFile +
+              "\n";
+          }
+          if (errors.type.length) {
+            string +=
+              Kooboo.text.common.File +
+              " " +
+              errors.type.join(", ") +
+              " " +
+              Kooboo.text.alert.fileUpload.invalidSuffix +
+              "\n";
+          }
+          if (errors.suffix.length) {
+            string +=
+              Kooboo.text.common.File +
+              " " +
+              errors.suffix.join(", ") +
+              " " +
+              Kooboo.text.alert.fileUpload.invalidType +
+              "\n";
+          }
+          return string;
+        }
+      }
+    });
+
+    function resetValue(el) {
+      $(el)
+        .wrap("<form>")
+        .parent("form")
+        .trigger("reset");
+      $(el).unwrap();
+    }
+  }
 });
 //#endregion </kb-upload>
 
 //#region <kb-modal>
-$(function() {
-  Vue.directive("kb-modal", {
-    bind: function(element, binding, vnode) {
-      $(element).on("hidden.bs.modal", function() {
-        vnode.context[binding.expression] = false; // sync binding.value = false;
-        if ($("body").find(".modal.in").length) {
-          if (!$("body").hasClass("modal-open")) {
-            $("body").addClass("modal-open");
-          }
+Vue.directive("kb-modal", {
+  bind: function(element, binding, vnode) {
+    $(element).on("hidden.bs.modal", function() {
+      vnode.context[binding.expression] = false; // sync binding.value = false;
+      if ($("body").find(".modal.in").length) {
+        if (!$("body").hasClass("modal-open")) {
+          $("body").addClass("modal-open");
         }
+      }
+    });
+    $(element).on("show.bs.modal", function() {
+      setTimeout(function() {
+        if (
+          $(element).hasClass("media-dialog") ||
+          $(element).hasClass("category-dialog")
+        ) {
+          $(".modal-backdrop:last").css("z-index", 200001);
+        }
+      }, 80);
+    });
+    $(element).on("shown.bs.modal", function() {
+      Kooboo.EventBus.publish("ko/binding/modal/shown", {
+        elem: element
       });
-      $(element).on("show.bs.modal", function() {
-        setTimeout(function() {
-          if (
-            $(element).hasClass("media-dialog") ||
-            $(element).hasClass("category-dialog")
-          ) {
-            $(".modal-backdrop:last").css("z-index", 200001);
-          }
-        }, 80);
-      });
-      $(element).on("shown.bs.modal", function() {
-        Kooboo.EventBus.publish("ko/binding/modal/shown", {
-          elem: element
-        });
-      });
-    },
-    update: function(element, binding) {
-      $(element).modal(binding.value ? "show" : "hide");
-    }
-  });
+    });
+  },
+  update: function(element, binding) {
+    $(element).modal(binding.value ? "show" : "hide");
+  }
 });
 //#endregion </kb-modal>
