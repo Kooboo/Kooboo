d0ee48df4687dcc12fd7f269ba0dd3ba687f8302
wenjie <wenjie@kooboo.cn>
2019-11-08 16:09:21 +0800

    Setting.html disable animate switch

--- Kooboo.Web/_Admin/Scripts/vue-components/kbSwitch.js
+++ Kooboo.Web/_Admin/Scripts/vue-components/kbSwitch.js
@@ -1,7 +1,6 @@
 (function() {
   Vue.component("kb-switch", {
-    template:
-      '<input type="checkbox" />',
+    template: '<input type="checkbox" />',
     props: {
       value: Boolean,
       onText: {
@@ -15,19 +14,17 @@
     },
     mounted: function() {
       var self = this;
-      self.$nextTick(function() {
-        $(self.$el).bootstrapSwitch("state", self.value, self.value);
-        $(self.$el).bootstrapSwitch("onText", self.onText);
-        $(self.$el).bootstrapSwitch("offText", self.offText);
-        $(self.$el).on("switchChange.bootstrapSwitch", function(event, state) {
-          self.$emit("input", state);
-        });
+      $(self.$el).bootstrapSwitch("state", self.value, self.value);
+      $(self.$el).bootstrapSwitch("onText", self.onText);
+      $(self.$el).bootstrapSwitch("offText", self.offText);
+      $(self.$el).on("switchChange.bootstrapSwitch", function(event, state) {
+        self.$emit("input", state);
       });
     },
     watch: {
       value: function(val) {
         $(this.$el).bootstrapSwitch("state", val, val);
-      },
+      }
     },
     beforeDestroy: function() {
       $(this.$el).bootstrapSwitch("destroy");
