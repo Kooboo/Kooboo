bfa4e16b40a531cb1846e73febbb8dbe77bcabee
wenjie <wenjie@kooboo.cn>
2019-11-14 17:36:05 +0800

    controltype

--- Kooboo.Web/_Admin/Scripts/vue-components/controlType/Selection.js
+++ Kooboo.Web/_Admin/Scripts/vue-components/controlType/Selection.js
@@ -1,10 +1,11 @@
 (function() {
-    var template = Kooboo.getTemplate("/_Admin/Scripts/components/controlType/Selection.html");
-
-    ko.components.register("selection", {
-        viewModel: function(params) {
-            _.assign(this, params);
-        },
-        template: template
-    })
-})()
+  Vue.component("kb-control-selection", {
+    template: Kooboo.getTemplate(
+      "/_Admin/Scripts/vue-components/controlType/Selection.html"
+    ),
+    props: {
+      field: Object
+    },
+    inject: ["kbFormItem"]
+  });
+})();
