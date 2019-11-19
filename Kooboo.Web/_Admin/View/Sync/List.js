41a0777d3e0b65fb86a6229a0c0c285a17782995
JongWong <jongwong@aliyun.com>
2019-11-19 20:45:42 +0800

    add page Sync/List

--- Kooboo.Web/_Admin/View/Sync/List.js
+++ Kooboo.Web/_Admin/View/Sync/List.js
@@ -1,220 +1,187 @@
 $(function() {
+  var self;
 
-    var settingId = Kooboo.getQueryString("id") || location.hash.split("#")[1];
-
-    var CONTIUNE_PULLING = true;
-
-    var syncModel = function() {
-        var self = this;
-
-        function pollingPull(senderVersion) {
-            if (CONTIUNE_PULLING) {
-                Kooboo.Publish.pull({
-                    id: Kooboo.getQueryString("id"),
-                    senderVersion: senderVersion
-                }).then(function(res) {
-                    if (!res.model.isFinish) {
-                        pollingPull(res.model.senderVersion);
-                    } else {
-                        self.isPulling(false);
-                        window.onbeforeunload = function() {};
-                        window.info.show(Kooboo.text.info.pull.success, true);
-                        self.changeTab("pull");
-                        DataCache.removeRelatedData("pull");
-                        Kooboo.SPA.beforeUnload = undefined;
-                    }
-                })
-            }
+  new Vue({
+    el: "#app",
+    data: function() {
+      return {
+        breads: [
+          {
+            name: Kooboo.text.component.breadCrumb.sites
+          },
+          {
+            name: Kooboo.text.component.breadCrumb.dashboard
+          },
+          {
+            name: Kooboo.text.common.KConfig,
+            url: Kooboo.Route.Get(Kooboo.Route.Publish.ListPage)
+          },
+          { name: Kooboo.text.site.sync.listName }
+        ],
+        settingId: Kooboo.getQueryString("id") || location.hash.split("#")[1],
+        tabIndex: 0,
+        isPulling: false,
+        isPushing: false,
+        pushItems: [],
+        selectedItems: [],
+        dynamicItems: [],
+        pager: undefined,
+        CONTIUNE_PULLING: true
+      };
+    },
+    created: function() {
+      self = this;
+      self.getPushItems();
+    },
+    watch: {
+      tabIndex: function(value) {
+        switch (value) {
+          case 0:
+            self.getPushItems();
+            break;
+          case 1:
+            self.getPullLog();
+            break;
+          case 2:
+            self.getPushLogs();
+            break;
         }
+      }
+    },
+    methods: {
+      pushToRemote: function() {
+        if (self.selectedItems.length > 0) {
+          self.isPushing = true;
+          var ids = [];
+          ids = self.selectedItems.map(function(item) {
+            return item.id;
+          });
 
-        self.isPulling = ko.observable(false);
-        self.isPushing = ko.observable(false);
-
-        self.PullToLocal = function(m, e) {
-            e.preventDefault();
-            window.info.show(Kooboo.text.info.startPulling, true);
-            pollingPull();
-            self.isPulling(true);
-
-            Kooboo.SPA.beforeUnload = function() {
-                var flag = false;
-
-                if (self.isPulling()) {
-                    flag = confirm(Kooboo.text.confirm.siteSynchronizing);
-                }
-
-                if (flag) {
-                    CONTIUNE_PULLING = false;
-                    return 'refresh';
-                }
-            }
-        }
-
-        self.pushToRemote = function() {
-
-            if (self.selectedItems().length) {
-                self.isPushing(true);
-                var logids = [];
-                _.forEach(self.selectedItems(), function(item) {
-                    logids.push(item.id);
-                });
-
-                Kooboo.Publish.push({
-                    id: settingId,
-                    logids: JSON.stringify(logids)
-                }).then(function(res) {
-
-                    if (res.success) {
-                        self.isPushing(false);
-                        self.changeTab("push");
-                        window.info.show(Kooboo.text.info.push.success, true);
-                        self.selectedItems.removeAll();
-                        localStorage.clear();
-                    } else {
-                        window.info.show(Kooboo.text.info.push.fail, false);
-                    }
-                })
+          Kooboo.Publish.push({
+            id: self.settingId,
+            logids: JSON.stringify(ids)
+          }).then(function(res) {
+            if (res.success) {
+              window.info.show(Kooboo.text.info.push.success, true);
+              self.selectedItems = [];
+              localStorage.clear();
+              self.getPushLogs(function() {
+                self.tabIndex = 2;
+              });
             } else {
-                alert(Kooboo.text.alert.selectBeforePushing);
+              window.info.show(Kooboo.text.info.push.fail, false);
             }
+            self.isPushing = false;
+          });
+        } else {
+          alert(Kooboo.text.alert.selectBeforePushing);
         }
-
-        this.selectItem = function(item) {
-            item.selected(!item.selected());
-            self.selectedItems[item.selected() ? "push" : "remove"](item);
-        }
-
-        this.selectedItems = ko.observableArray();
-
-        this.getChangeClass = function(type) {
-            var _class = "";
-            switch (type.toLowerCase()) {
-                case 'add':
-                    _class = 'green'
-                    break;
-                case 'update':
-                    _class = 'blue';
-                    break;
-                case 'delete':
-                    _class = 'red';
-                    break
+      },
+      getPushItems: function() {
+        Kooboo.Publish.pushItems({
+          id: self.settingId
+        }).then(function(res) {
+          if (res.success) {
+            self.pushItems = res.model;
+          }
+        });
+      },
+      getPushLogs: function(callback) {
+        Kooboo.Publish.pushLog({
+          SyncSettingId: self.settingId
+        }).then(function(res) {
+          if (res.success) {
+            self.dynamicItems = res.model.list;
+            self.pager = res.model;
+          }
+          if (callback) {
+            callback();
+          }
+        });
+      },
+      pollingPull: function(senderVersion) {
+        if (self.CONTIUNE_PULLING) {
+          Kooboo.Publish.pull({
+            id: Kooboo.getQueryString("id"),
+            senderVersion: senderVersion
+          }).then(function(res) {
+            if (!res.model.isFinish) {
+              self.pollingPull(res.model.senderVersion);
+            } else {
+              self.isPulling = false;
+              window.onbeforeunload = function() {};
+              window.info.show(Kooboo.text.info.pull.success, true);
+              self.tabIndex = 1;
+              Kooboo.SPA.beforeUnload = undefined;
             }
-            return _class;
+          });
         }
+      },
+      onPullToLocal: function() {
+        window.info.show(Kooboo.text.info.startPulling, true);
+        self.pollingPull();
+        self.isPulling = true;
 
-        this.getDisplayTime = function(t) {
-            var d = new Date(t);
-            return d.toDefaultLangString();
-        }
+        Kooboo.SPA.beforeUnload = function() {
+          var flag = false;
 
-        this.localChanges = ko.observableArray();
+          if (self.isPulling()) {
+            flag = confirm(Kooboo.text.confirm.siteSynchronizing);
+          }
 
-        this.dynamicItems = ko.observableArray();
-
-        this.pager = ko.observable();
-
-        this.tabs = ko.observable([{
-            displayName: Kooboo.text.site.sync.localChanges,
-            value: "local"
-        }, {
-            displayName: Kooboo.text.site.sync.pullLog,
-            value: "pull"
-        }, {
-            displayName: Kooboo.text.site.sync.pushLog,
-            value: "push"
-        }]);
-
-        this.curTab = ko.observable();
-
-        this.changeTab = function(tab) {
-            if (tab !== self.curTab()) {
-
-                switch (tab) {
-                    case "local":
-                        Kooboo.Publish.pushItems({
-                            id: settingId
-                        }).then(function(res) {
-                            if (res.success) {
-                                var _list = [];
-                                _.forEach(res.model, function(change) {
-                                    ko.mapping.fromJS(change);
-
-                                    change.selected = ko.observable(false);
-
-                                    _list.push(change);
-                                })
-                                self.localChanges(_list);
-                            }
-                        })
-                        self.curTab(tab);
-                        break;
-                    case "push":
-                        self.allSelected(false);
-                        Kooboo.Publish.pushLog({
-                            SyncSettingId: settingId
-                        }).then(function(res) {
-
-                            if (res.success) {
-                                self.dynamicItems(res.model.list);
-                                self.pager(res.model);
-                                self.curTab(tab);
-                            }
-                        });
-                        break;
-                    case "pull":
-                        self.allSelected(false);
-                        Kooboo.Publish.pullLog({
-                            SyncSettingId: settingId
-                        }).then(function(res) {
-
-                            if (res.success) {
-                                self.dynamicItems(res.model.list);
-                                self.pager(res.model);
-                                self.curTab(tab);
-                            }
-                        })
-                        break;
-                }
+          if (flag) {
+            self.CONTIUNE_PULLING = false;
+            return "refresh";
+          }
+        };
+      },
+      getPullLog: function(callback) {
+        Kooboo.Publish.pullLog({
+          SyncSettingId: self.settingId
+        }).then(function(res) {
+          if (res.success) {
+            self.dynamicItems = res.model.list;
+            self.pager = res.model;
+            if (callback) {
+              callback();
             }
+          }
+        });
+      },
+      getChangeClass: function(type) {
+        var _class = "";
+        switch (type.toLowerCase()) {
+          case "add":
+            _class = "green";
+            break;
+          case "update":
+            _class = "blue";
+            break;
+          case "delete":
+            _class = "red";
+            break;
         }
-
-        this.allSelected = ko.pureComputed({
-            read: function() {
-                if (self.localChanges().length === 0) {
-                    return false;
-                }
-                return self.localChanges().length == self.selectedItems().length;
-            },
-            write: function(value) {
-
-                if (typeof value == "boolean") {
-                    self.selectedItems.removeAll();
-
-                    _.forEach(self.localChanges(), function(change) {
-                        change.selected(value);
-                        value && self.selectedItems.push(change);
-                    })
-                }
-            },
-            owner: this
-        })
-
-        Kooboo.EventBus.subscribe("kb/pager/change", function(page) {
-            Kooboo.Publish[self.curTab() + "Log"]({
-                SyncSettingId: settingId,
-                pageNr: page
-            }).then(function(res) {
-
-                if (res.success) {
-                    self.dynamicItems(res.model.list);
-                    self.pager(res.model);
-                }
-            })
-        })
-    };
-
-    var vm = new syncModel();
-    ko.applyBindings(vm, document.getElementById("main"));
-
-    vm.changeTab("local");
+        return _class;
+      },
+      changePage: function(index) {
+        var name = undefined;
+        if (self.tabIndex === 1) {
+          name = "pullLog";
+        } else if (self.tabIndex === 2) {
+          name = "pushLog";
+        }
+        if (name) {
+          Kooboo.Publish[name]({
+            SyncSettingId: self.settingId,
+            pageNr: index
+          }).then(function(res) {
+            if (res.success) {
+              self.dynamicItems = res.model.list;
+              self.pager = res.model;
+            }
+          });
+        }
+      }
+    }
+  });
 });
