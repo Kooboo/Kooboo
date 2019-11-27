a544df9912e9d26ec88577f0e99e125712deef38
wenjie <wenjie@kooboo.cn>
2019-11-27 10:22:41 +0800

    kb-html-tree done

--- Kooboo.Web/_Admin/Scripts/vue-components/htmlViewer/kbHtmlTree.js
+++ Kooboo.Web/_Admin/Scripts/vue-components/htmlViewer/kbHtmlTree.js
@@ -1,129 +1,115 @@
 (function() {
-
-    var SELF_CLOSE_TAGS = [
-        "area", "base", "br", "col",
-        "command", "embed", "hr", "img",
-        "input", "keygen", "link", "meta",
-        "param", "source", "track", "wbr"
-    ];
-    var template = Kooboo.getTemplate("/_Admin/Scripts/components/htmlViewer/kbHtmlTree.html");
-
-    ko.components.register("kb-html-tree", {
-        viewModel: function(params) {
-
-            /*
-             * IN:
-             * parmas: {
-             *     elem: ELEMENT,
-             *     depth: (number)
-             * }
-             * 
-             * OUT:
-             * TREE_MOUSE_CLICK: {
-             *      event: "kb/html/elem/change",
-             *      value: ELEMENT
-             * },
-             * PATH_MOUSE_HOVER: {
-             *      event: "kb/html/elem/hover",
-             *      value: ELEMENT
-             * }
-             * 
-             */
-
-            var self = this;
-
-            this.rootElem = ko.observable(params && params.rootElem);
-
-            // Display-DOM-tree array
-            this.tree = ko.observableArray();
-
-            if (params && params.elem) {
-
-                if (typeof params.elem == "object") {
-                    if (self.rootElem()) {
-                        if ($.contains(self.rootElem(), params.elem) ||
-                            $(self.rootElem()).is($(params.elem))) {
-                            self.tree([new elemModel(params.elem, params.depth)]);
-                        } else {
-                            self.tree([new elemModel(self.rootElem(), params.depth)]);
-                        }
-                    } else {
-                        self.tree([new elemModel(params.elem, params.depth)]);
-                    }
-                } else {
-                    self.tree(params.elem());
-                }
+  var SELF_CLOSE_TAGS = [
+    "area",
+    "base",
+    "br",
+    "col",
+    "command",
+    "embed",
+    "hr",
+    "img",
+    "input",
+    "keygen",
+    "link",
+    "meta",
+    "param",
+    "source",
+    "track",
+    "wbr"
+  ];
+  var self;
+  Vue.component("kb-html-tree", {
+    template: Kooboo.getTemplate(
+      "/_Admin/Scripts/vue-components/htmlViewer/kbHtmlTree.html"
+    ),
+    props: {
+      elem: [Object, Array],
+      rootElem: [Object, Array],
+      depth: Number
+    },
+    data: function() {
+      self = this;
+      return {
+        tree: [],
+        maxDepth: 5
+      };
+    },
+    mounted: function() {
+      if (self.elem) {
+        if (typeof self.elem == "object") {
+          if (self.rootElem) {
+            if (
+              $.contains(self.rootElem, self.elem) ||
+              $(self.rootElem).is($(self.elem))
+            ) {
+              self.tree = [new elemModel(self.elem, self.depth)];
+            } else {
+              self.tree = [new elemModel(self.rootElem, self.depth)];
             }
-            this.maxDepth = ko.observable(5);
-
-            // Modified the display-DOM-tree depth
-            this.depth = ko.observable(params && params.depth);
-
-            this.changeElem = function(m) {
-                Kooboo.EventBus.publish("kb/lighter/holder", m.elem());
-            }
-
-            this.hoverElem = function(m) {
-                m.isHovered(true);
-                Kooboo.EventBus.publish("kb/html/elem/hover", m.elem());
-            }
-
-            this.unhoverElem = function(m) {
-                m.isHovered(false);
-            }
-
-        },
-        template: template
-    });
-
-    var elemModel = function(elem, depth) {
-
-        var self = this;
-
-        this.elem = ko.observable(elem);
-
-        this.depth = ko.observable(depth);
-
-        this.name = ko.observable(elem.tagName.toLowerCase());
-
-        this.isSelfClosed = ko.observable(SELF_CLOSE_TAGS.indexOf(self.name()) > -1);
-
-        this.attr = ko.observable();
-
-        this.hasAttr = ko.observable(true);
-
-        if ($(elem).attr("k-placeholder")) {
-            self.attr({ key: "placeholder", value: $(elem).attr("k-placeholder") });
-        } else if ($(elem).attr("id")) {
-            self.attr({ key: "id", value: $(elem).attr("id") });
-        } else if ($(elem).attr("class")) {
-            self.attr({ key: "class", value: $(elem).attr("class") });
+          } else {
+            self.tree = [new elemModel(self.elem, self.depth)];
+          }
         } else {
-            self.hasAttr(false);
-        };
-
-        this.attrText = ko.pureComputed(function() {
-            return '<span class="attr-name">' + self.attr().key + '</span>="<span class="attr-value">' + self.attr().value + '</span>"';
-        })
-
-        this.hasChild = ko.observable($(elem).children().length > 0);
-
-        if (elem.hasAttribute("k-placeholder") && elem.hasAttribute("k-omit")) {
-            this.hasChild(false);
+          self.tree = self.elem;
         }
-
-        this.getChildren = function(elem) {
-            var _children = [];
-            $(elem).children().each(function(idx, el) {
-                if (["meta", "style", "script"].indexOf(el.tagName.toLowerCase()) == -1) {
-                    _children.push(new elemModel(el, self.depth() + 1));
-                }
-            })
-            return _children;
-        }
-
-        this.children = ko.observableArray(self.getChildren(elem));
-
-        this.isHovered = ko.observable(false);
+      }
+    },
+    methods: {
+      changeElem: function(m) {
+        Kooboo.EventBus.publish("kb/lighter/holder", m.elem);
+        self.$emit("change", m);
+      },
+      hoverElem: function(m) {
+        m.isHovered = true;
+        Kooboo.EventBus.publish("kb/html/elem/hover", m.elem);
+        self.$emit("hover", m);
+      },
+      unhoverElem: function(m) {
+        m.isHovered = false;
+      }
     }
+  });
+
+  function elemModel(elem, depth) {
+    this.elem = elem;
+    this.depth = depth;
+    this.name = elem.tagName.toLowerCase();
+    this.isSelfClosed = SELF_CLOSE_TAGS.indexOf(self.name) > -1;
+    this.attr = "";
+    this.hasAttr = true;
+
+    if ($(elem).attr("k-placeholder")) {
+      this.attr = { key: "placeholder", value: $(elem).attr("k-placeholder") };
+    } else if ($(elem).attr("id")) {
+      this.attr = { key: "id", value: $(elem).attr("id") };
+    } else if ($(elem).attr("class")) {
+      this.attr = { key: "class", value: $(elem).attr("class") };
+    } else {
+      this.hasAttr = false;
+    }
+    this.attrText = "";
+    if (this.hasAttr) {
+      '<span class="attr-name">' +
+        self.attr.key +
+        '</span>="<span class="attr-value">' +
+        self.attr.value +
+        '</span>"';
+    }
+    this.hasChild = $(elem).children().length > 0;
+    if (elem.hasAttribute("k-placeholder") && elem.hasAttribute("k-omit")) {
+      this.hasChild = false;
+    }
+    this.children = [];
+    $(elem)
+      .children()
+      .each(function(idx, el) {
+        if (
+          ["meta", "style", "script"].indexOf(el.tagName.toLowerCase()) == -1
+        ) {
+          this.children.push(new elemModel(el, self.depth + 1));
+        }
+      });
+
+    this.isHovered = false;
+  }
 })();
