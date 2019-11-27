c6463cd42f5904bb11c49806967ed049952671cb
wenjie <wenjie@kooboo.cn>
2019-11-27 14:57:04 +0800

    fixed: kb-html-viewer make element observable

--- Kooboo.Web/_Admin/Scripts/vue-components/htmlViewer/kbHtmlTree.js
+++ Kooboo.Web/_Admin/Scripts/vue-components/htmlViewer/kbHtmlTree.js
@@ -23,35 +23,38 @@
       "/_Admin/Scripts/vue-components/htmlViewer/kbHtmlTree.html"
     ),
     props: {
-      elem: [Object, Array],
-      rootElem: [Object, Array],
+      elem: [HTMLElement, Array],
+      rootElem: HTMLElement,
       depth: Number
     },
     data: function() {
       self = this;
       return {
-        tree: [],
         maxDepth: 5
       };
     },
-    mounted: function() {
-      if (self.elem) {
-        if (typeof self.elem == "object") {
-          if (self.rootElem) {
-            if (
-              $.contains(self.rootElem, self.elem) ||
-              $(self.rootElem).is($(self.elem))
-            ) {
-              self.tree = [new elemModel(self.elem, self.depth)];
-            } else {
-              self.tree = [new elemModel(self.rootElem, self.depth)];
-            }
+    computed: {
+      tree: function() {
+        var tree = [];
+        if (self.elem) {
+          if (Array.isArray(self.elem)) {
+            tree = self.elem;
           } else {
-            self.tree = [new elemModel(self.elem, self.depth)];
+            if (self.rootElem) {
+              if (
+                $.contains(self.rootElem, self.elem) ||
+                $(self.rootElem).is($(self.elem))
+              ) {
+                tree = [new elemModel(self.elem, self.depth)];
+              } else {
+                tree = [new elemModel(self.rootElem, self.depth)];
+              }
+            } else {
+              tree = [new elemModel(self.elem, self.depth)];
+            }
           }
-        } else {
-          self.tree = self.elem;
         }
+        return tree;
       }
     },
     methods: {
@@ -71,45 +74,47 @@
   });
 
   function elemModel(elem, depth) {
-    this.elem = elem;
-    this.depth = depth;
-    this.name = elem.tagName.toLowerCase();
-    this.isSelfClosed = SELF_CLOSE_TAGS.indexOf(self.name) > -1;
-    this.attr = "";
-    this.hasAttr = true;
+    var _this = this;
+    _this.elem = elem;
+    _this.depth = depth;
+    _this.name = elem.tagName.toLowerCase();
+    _this.isSelfClosed = SELF_CLOSE_TAGS.indexOf(_this.name) > -1;
+    _this.attr = "";
+    _this.hasAttr = true;
 
     if ($(elem).attr("k-placeholder")) {
-      this.attr = { key: "placeholder", value: $(elem).attr("k-placeholder") };
+      _this.attr = { key: "placeholder", value: $(elem).attr("k-placeholder") };
     } else if ($(elem).attr("id")) {
-      this.attr = { key: "id", value: $(elem).attr("id") };
+      _this.attr = { key: "id", value: $(elem).attr("id") };
     } else if ($(elem).attr("class")) {
-      this.attr = { key: "class", value: $(elem).attr("class") };
+      _this.attr = { key: "class", value: $(elem).attr("class") };
     } else {
-      this.hasAttr = false;
+      _this.hasAttr = false;
     }
-    this.attrText = "";
-    if (this.hasAttr) {
-      '<span class="attr-name">' +
-        self.attr.key +
+    _this.attrText = "";
+    if (_this.hasAttr) {
+      _this.attrText =
+        '<span class="attr-name">' +
+        _this.attr.key +
         '</span>="<span class="attr-value">' +
-        self.attr.value +
+        _this.attr.value +
         '</span>"';
     }
-    this.hasChild = $(elem).children().length > 0;
+    _this.hasChild = $(elem).children().length > 0;
     if (elem.hasAttribute("k-placeholder") && elem.hasAttribute("k-omit")) {
-      this.hasChild = false;
+      _this.hasChild = false;
     }
-    this.children = [];
+    _this.children = [];
     $(elem)
       .children()
       .each(function(idx, el) {
         if (
           ["meta", "style", "script"].indexOf(el.tagName.toLowerCase()) == -1
         ) {
-          this.children.push(new elemModel(el, self.depth + 1));
+          _this.children.push(new elemModel(el, _this.depth + 1));
         }
       });
 
-    this.isHovered = false;
+    _this.isHovered = false;
   }
 })();
