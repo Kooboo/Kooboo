7a3a2bca68e98697f8e20ab6e57372b6c0888c13
JongWong <jongwong@aliyun.com>
2019-11-28 19:25:24 +0800

    page menu codehelp done

--- Kooboo.Web/_Admin/View/Development/Menu.js
+++ Kooboo.Web/_Admin/View/Development/Menu.js
@@ -1,5 +1,6 @@
 $(function() {
   var self;
+
   new Vue({
     el: "#app",
     data: function() {
@@ -21,15 +22,35 @@ $(function() {
             name: name
           }
         ],
+        mark:{markAnchorText: "{anchortext}",
+          markHref: "{href}",
+          markSubItems: "{items}",
+          markActiveClass: "{activeclass:className}",
+          markParentId: "{parentid}",
+          markCurrentId: "{currentid}"},
+        editorOptions:{lineNumbersMinChars:2,minimap:{enabled:false}},
         defaultLang: undefined,
-        isMultiLangeMenu: undefined,
+        isMultiLangMenu: true,
+        showMultiLangModal: false,
+        showTemplateModal: false,
+        multiNames:undefined,
         loading: undefined,
-        _menuItems: undefined
+        CUR_MENU: undefined,
+        menuWithNoChild: false,
+        showCurrentTemplate: false,
+        currentTemplate: '',
+        previewCode: undefined,
+        subItemContainer: '',
+        subItemTemplate:""
+
+
+
       };
     },
     created: function() {
       self = this;
       self.getData();
+      self.initHelp();
     },
     mounted: function() {},
     methods: {
@@ -178,6 +199,63 @@ $(function() {
         );
         self.getData();
       },
+      initHelp: function() {
+        self.codeHelperSubItem = [{
+          text: "MarkSubItems",
+          value: self.mark.markSubItems,
+          for: "SUB_ITEM_CONTAINER"
+        }];
+        self.subTmplCodeHelpers = [{
+          text: "MarkAnchorText",
+          value: self.mark.markAnchorText,
+          for: "SUB_ITEM_TEMPLATE"
+        }, {
+          text: "MarkHref",
+          value: self.mark.markHref,
+          for: "SUB_ITEM_TEMPLATE"
+        }, {
+          text: "MarkSubItems",
+          value: self.mark.markSubItems,
+          for: "SUB_ITEM_TEMPLATE"
+        }, {
+          text: "MarkActiveClass",
+          value: self.mark.markActiveClass,
+          for: "SUB_ITEM_TEMPLATE"
+        }, {
+          text: "MarkParentId",
+          value: self.mark.markParentId,
+          for: "SUB_ITEM_TEMPLATE"
+        }, {
+          text: "MarkCurrentId",
+          value: self.mark.markCurrentId,
+          for: "SUB_ITEM_TEMPLATE"
+        }];
+            self.tmplCodeHelpers = [{
+          text: "MarkAnchorText",
+          value: self.mark.markAnchorText,
+          for: "TEMPLATE"
+        }, {
+          text: "MarkHref",
+          value: self.mark.markHref,
+          for: "TEMPLATE"
+        }, {
+          text: "MarkSubItems",
+          value: self.mark.markSubItems,
+          for: "TEMPLATE"
+        }, {
+          text: "MarkActiveClass",
+          value: self.mark.markActiveClass,
+          for: "TEMPLATE"
+        }, {
+          text: "MarkParentId",
+          value: self.mark.markParentId,
+          for: "TEMPLATE"
+        }, {
+          text: "MarkCurrentId",
+          value: self.mark.markCurrentId,
+          for: "TEMPLATE"
+        }]
+      },
       menuMoveDown: function(e, index, item) {
         var count = self.getAllChildrenMumber(item);
         self.swapOrder(
@@ -235,7 +313,189 @@ $(function() {
         } else {
           window.info.show(Kooboo.text.info.menuNameRequired, false);
         }
+      },
+      hideMultiLangModal:function() {
+        self.showMultiLangModal = false;
+        self.CUR_MENU = undefined;
+      },
+      onShowMultiModal: function (event,item) {
+        self.CUR_MENU = item;
+        Kooboo.Menu.getLang({
+          id: item.id,
+          rootId: item.rootId
+        }).then(function(res) {
+          if (res.success) {
+
+            var keys = Object.keys(res.model);
+            var defaultCultureIdx = keys.indexOf(self.defaultLang);
+
+            self.multiNames = [];
+            keys.forEach(function (key,index) {
+              self.multiNames.push({key:key,value:res.model[key]});
+                  if(index === defaultCultureIdx)
+                    self.multiNames[index].validate={valid:true,msg:''}
+            });
+            self.showMultiLangModal = true;
+          }
+        })
+      },
+      onSaveMultiName:function () {
+        var hasError = false;
+        self.multiNames.forEach(function (item) {
+          if(item.validate) {
+            item.validate = Kooboo.validField(item.value,[{ required: Kooboo.text.validation.required }])
+            if(item.validate.valid === false) {
+              hasError = true
+            }
+          }
+        });
+        self.$forceUpdate();
+        if(!hasError) {
+          var menus = _.cloneDeep(self.menuItems),
+              data = {};
+          _.forEach(self.multiNames, function(mn) {
+            data[mn.key] = mn.value;
+          });
+          Kooboo.Menu.updateLang({
+            id: self.CUR_MENU.id,
+            rootId: self.CUR_MENU.rootId,
+            values: data
+          }).then(function(res) {
+            if (res.success) {
+              self.hideMultiLangModal();
+              self.getData()
+            } else {
+              window.info.show(Kooboo.text.info.save.fail, false);
+            }
+          })
+        }
+      },
+      renderPreview:function(tmpl, context) {
+        
+        if (typeof  tmpl ==='string'  && tmpl.indexOf(self.mark.markHref) > -1) {
+          tmpl = tmpl.split(self.mark.markHref).join(context.url ? context.url : "#");
+        }
+        if (typeof  tmpl ==='string'  && tmpl.indexOf(self.mark.markAnchorText) > -1) {
+          tmpl = tmpl.split(self.mark.markAnchorText).join(context.name);
+        }
+        if (typeof  tmpl ==='string'  && tmpl.indexOf(self.mark.markCurrentId) > -1) {
+          tmpl = tmpl.split(self.mark.markCurrentId).join(context.id);
+        }
+        if (typeof  tmpl ==='string'  && tmpl.indexOf(self.mark.markParentId) > -1) {
+          tmpl = tmpl.split(self.mark.markParentId).join(context.parentId);
+        }
+        
+        if (typeof  tmpl ==='string'  && tmpl.indexOf(self.mark.markSubItems) > -1) {
+          
+          if (context.children.length > 0) {
+            var _tmplArr = tmpl.split(self.mark.markSubItems),
+                parentsTemplate = "";
+
+            
+            tmpl = _tmplArr.join((self.CUR_MENU.id === context.id) ? self.subItemContainer : context.subItemContainer);
+            parentsTemplate = (self.CUR_MENU.id === context.id) ? self.subItemTemplate : context.subItemTemplate;
+
+            var childTemplates = [];
+            
+            _.forEach(context.children, function(child) {
+              var template = ((self.CUR_MENU.id === child.id) ?
+                  self.currentTemplate :
+                  (child.template === context.subItemTemplate ? parentsTemplate : child.template)) ||
+                  parentsTemplate;
+              childTemplates.push(self.renderPreview(template, child));
+            });
+            
+            tmpl = tmpl.split(self.mark.markSubItems).join(childTemplates.join(""));
+            
+          } else {
+            tmpl = tmpl.split(self.mark.markSubItems).join("");
+            
+          }
+        }
+        if (typeof  tmpl ==='string'  &&tmpl.indexOf("activeclass:") > -1) {
+          try {
+            function setActiveClass(elem) {
+              var selfTemplate = $(elem)[0].outerHTML;
+              var hasActiveClass = selfTemplate.match(/{.*?}/);
+              if (hasActiveClass) {
+                var className = hasActiveClass[0].match(/{[\w\W]*:([\w\W]*)}/)[1];
+                if ($(elem)[0].hasAttribute("{activeclass:" + className + "}")) {
+                  $(elem)[0].removeAttribute("{activeclass:" + className + "}");
+                  $(elem).addClass(className);
+                }
+
+                $(elem).children().each(function(idx, child) {
+                  setActiveClass($(child));
+                })
+              }
+              return elem;
+            }
+            var elem = setActiveClass($(tmpl));
+            tmpl = elem[0].outerHTML;
+
+          } catch (ex) {
+            tmpl = "error";
+          }
+        }
+        
+        return tmpl;
+      },
+      onHideTemplateModal:function () {
+        self.showTemplateModal = false;
+        self.CUR_MENU = undefined;
+      },
+      onShowTemplateModal:function(event,item){
+        self.CUR_MENU = item;
+        var _find = _.findLast(self.menuItems, function(menu) {
+          return menu.id === self.CUR_MENU.id;
+        });
+
+        self.subItemContainer = _find.subItemContainer || "";
+        self.subItemTemplate = _find.subItemTemplate || "";
+        self.currentTemplate = _find.template || "";
+        if (!self.currentTemplate) {
+          var parent = _.find(self.menuItems, function(menu) {
+            return menu.id === _find.parentId;
+          });
+          if(parent) {
+            self.currentTemplate = parent.subItemTemplate;
+          }
+
+        }
+
+        var current = self.mark.markSubItems;
+        self.previewCode = html_beautify(self.renderPreview(current, self.menuItems[0]));
+       self.templatePreview = item.subItemTemplate;
+        if(item.children && item.children.length === 0){
+          self.menuWithNoChild = true
+        }
+        if(item.id !== item.rootId){
+          self.showCurrentTemplate = true
+        }
+        self.showTemplateModal = true
+      },
+      onSaveTemplate:function () {
+
+      },
+      codeHelp:function (event,m) {
+
+        var component = undefined;
+
+        switch (m.for) {
+          case "SUB_ITEM_CONTAINER":
+            component = self.$refs.editor2;
+                break;
+          case "SUB_ITEM_TEMPLATE":
+            component = self.$refs.editor3;
+            break;
+          case "TEMPLATE":
+            component = self.$refs.editor4;
+            break;
+        }
+        component.replace(m.value)
+
       }
     }
   });
+
 });
