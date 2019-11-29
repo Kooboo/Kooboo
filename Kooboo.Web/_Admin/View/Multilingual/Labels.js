2abc705567c892cd321635beb80186fa6f67b24d
JongWong <jongwong@aliyun.com>
2019-11-29 17:16:03 +0800

    page Multilingual/Labels done

--- Kooboo.Web/_Admin/View/Multilingual/Labels.js
+++ Kooboo.Web/_Admin/View/Multilingual/Labels.js
@@ -1,103 +1,107 @@
 $(function() {
+  var self;
+  new Vue({
+    el: "#app",
+    data: function() {
+      return {
+        langName: undefined,
+        multiLang: Kooboo.getQueryString("lang"),
+        mutliLangName: undefined,
+        breads: [],
+        labels: [],
+        showEditModal: false,
+        editingLabel: undefined,
+        editingLabelIndex: undefined,
+        newMultiLangValue: ""
+      };
+    },
+    created: function() {
+      self = this;
+      self.getData();
+    },
+    watch: {
+      mutliLangName: function() {
+        self.breads = [
+          {
+            name: "SITES"
+          },
+          {
+            name: "DASHBOARD"
+          },
+          {
+            name: self.mutliLangName,
+            url: "#"
+          },
+          {
+            name: Kooboo.text.common.HTMLblocks
+          }
+        ];
+      },
+      labels: function() {}
+    },
+    methods: {
+      getData: function() {
+        $.when(Kooboo.Site.Langs(), Kooboo.Label.getList()).then(function(
+          r1,
+          r2
+        ) {
+          var langRes = r1[0],
+            labelRes = r2[0];
+          if (langRes.success && labelRes.success) {
+            self.lang = langRes.model.default;
+            self.langName = langRes.model.defaultName;
+            self.mutliLangName = langRes.model.cultures[self.multiLang];
 
-    var labelViewModel = function() {
+            var temp = [];
+            labelRes.model.forEach(function(item) {
+              item.defaultLang = self.lang;
+              item.multiLang = self.multiLang;
 
-        var self = this;
+              var _d = new Date(item.lastModified);
+              item.date = _d.toDefaultLangString();
+              item.defaultValue = item.values[item.defaultLang];
+              item.multilingual = item.values[item.multiLang];
+              temp.push(item);
+            });
+            self.labels = temp;
+          }
+        });
+      },
+      editLabel: function(event, item, index) {
+        self.editingLabel = item;
+        self.editingLabelIndex = index;
+        self.newMultiLangValue = item.multilingual;
+        self.showEditModal = true;
+        setTimeout(function() {
+          $(".autosize")
+            .textareaAutoSize()
+            .trigger("keyup");
+        }, 300);
+      },
+      onHideEditModal: function() {
+        self.editingLabel = undefined;
+        self.showEditModal = false;
+        self.editingLabelIndex = undefined;
+        self.newMultiLangValue = "";
+      },
+      onSaveEditModal: function() {
+        var values = {};
 
-        this.lang = ko.observable();
-        this.langName = ko.observable();
+        values[self.multiLang] = self.newMultiLangValue || "";
 
-        this.multiLang = ko.observable();
-        this.mutliLangName = ko.observable();
-
-        this.labels = ko.observableArray();
-
-        this.rendered = function() {
-            waterfall('.label-list')
-        }
-
-        this.editingLabel = ko.observable();
-
-        this.newMultiLangValue = ko.observable();
-
-        this.editLabel = function(m, e) {
-            self.showEditModal(true);
-            self.editingLabel(m);
-
-            self.newMultiLangValue(m.multilingual());
-
-            setTimeout(function() {
-                $(".autosize").textareaAutoSize().trigger("keyup");
-            }, 300);
-        }
-
-        $.when(Kooboo.Site.Langs(),
-                Kooboo.Label.getList())
-            .then(function(r1, r2) {
-
-                var langRes = r1[0],
-                    labelRes = r2[0];
-
-                if (langRes.success && labelRes.success) {
-
-                    self.lang(langRes.model.default);
-                    self.langName(langRes.model.defaultName);
-                    self.multiLang(Kooboo.getQueryString("lang"));
-                    self.mutliLangName(langRes.model.cultures[self.multiLang()]);
-
-                    labelRes.model.forEach(function(label) {
-                        label.defaultLang = self.lang();
-                        label.multiLang = self.multiLang();
-                        self.labels.push(new MultilingualLabelModel(label));
-                    })
-                }
-            })
-
-        this.showEditModal = ko.observable(false);
-
-        this.onHideEditModal = function() {
-            self.showEditModal(false);
-            self.newMultiLangValue('');
-        }
-
-        this.onSaveEditModal = function() {
-            var values = {};
-
-            values[self.multiLang()] = self.newMultiLangValue() || "";
-
-            Kooboo.Label.Update({
-                id: self.editingLabel().id(),
-                values: values
-            }).then(function(res) {
-                if (res.success) {
-                    self.editingLabel().multilingual(self.newMultiLangValue());
-                    self.editingLabel().date(new Date().toDefaultLangString());
-                    self.onHideEditModal();
-                    window.info.done(Kooboo.text.info.update.success);
-                }
-            })
-        }
+        Kooboo.Label.Update({
+          id: self.editingLabel.id,
+          values: values
+        }).then(function(res) {
+          if (res.success) {
+            self.editingLabel.multilingual = self.newMultiLangValue;
+            self.editingLabel.date = new Date().toDefaultLangString();
+            self.labels[self.editingLabelIndex] = self.editingLabel;
+            self.onHideEditModal();
+            window.info.done(Kooboo.text.info.update.success);
+          }
+        });
+      }
     }
-
-    var vm = new labelViewModel();
-
-    var MultilingualLabelModel = function(info) {
-        var self = this;
-
-        var _d = new Date(info.lastModified);
-
-        ko.mapping.fromJS(info, {}, self);
-
-        this.date = ko.observable(_d.toDefaultLangString());
-
-        this.defaultValue = ko.observable(info.values[info.defaultLang]);
-
-        this.multilingual = ko.observable(info.values[info.multiLang]);
-    }
-
-    ko.applyBindings(vm, document.getElementById("main"));
-
-    $(window).on('resize', function() {
-        waterfall('.label-list')
-    })
+  });
 });
