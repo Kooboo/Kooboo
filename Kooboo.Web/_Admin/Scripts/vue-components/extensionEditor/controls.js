089f562ae8d1bdb4b4f9fe08fab27de4f19ee396
wenjie <wenjie@kooboo.cn>
2019-12-04 10:55:07 +0800

    debouce

--- Kooboo.Web/_Admin/Scripts/vue-components/extensionEditor/controls.js
+++ Kooboo.Web/_Admin/Scripts/vue-components/extensionEditor/controls.js
@@ -1,43 +1,142 @@
 (function() {
-  Vue.component("control-string", {
-    template: Kooboo.getTemplate(
-      "/_Admin/Scripts/vue-components/extensionEditor/string.html"
-    ),
-    inheritAttrs: false,
-    props: ["name", "value"],
+  var controlBase = Vue.component("controls-base", {
+    props: ["name", "value", "fields"],
     methods: {
       change: function(value) {
+        console.log(this.name, value);
         this.$emit("change", {
           name: this.name,
           value: value
         });
       }
     }
+  });
+
+  Vue.component("control-string", {
+    template: Kooboo.getTemplate(
+      "/_Admin/Scripts/vue-components/extensionEditor/string.html"
+    ),
+    extends: controlBase
   });
 
   Vue.component("control-textarea", {
     template: Kooboo.getTemplate(
       "/_Admin/Scripts/vue-components/extensionEditor/textarea.html"
     ),
-    inheritAttrs: false,
-    props: ["name", "value"],
+    extends: controlBase
+  });
+
+  Vue.component("control-checkbox", {
+    template: Kooboo.getTemplate(
+      "/_Admin/Scripts/vue-components/extensionEditor/checkbox.html"
+    ),
+    extends: controlBase,
+    computed: {
+      checked: function() {
+        return this.value === "True" || this.value === "true";
+      }
+    }
+  });
+
+  Vue.component("control-order", {
+    template: Kooboo.getTemplate(
+      "/_Admin/Scripts/vue-components/extensionEditor/orderBy.html"
+    ),
+    extends: controlBase
+  });
+
+  Vue.component("control-filter", {
+    template: Kooboo.getTemplate(
+      "/_Admin/Scripts/vue-components/extensionEditor/filter.html"
+    ),
+    extends: controlBase,
+    data: function() {
+      return {
+        values: []
+      };
+    },
+    created: function() {
+      var self = this;
+      this.debounceChange = _.debounce(function() {
+        if (self.fields && self.fields.length) {
+          self.change(self.values);
+        } else {
+          self.change([]);
+        }
+      }, 400);
+    },
     methods: {
-      change: function(value) {
-        this.$emit("change", {
-          name: this.name,
-          value: value
-        });
+      selectField: function(filter) {
+        if (this.fields) {
+          var choosedOperator = this.fields.filter(function(item) {
+            return item.name === filter.key;
+          })[0];
+          if (choosedOperator) {
+            filter.operators = choosedOperator.operators;
+            if (
+              !filter.comparison ||
+              filter.operators.indexOf(filter.comparison) === -1
+            ) {
+              filter.comparison = filter.operators[0];
+            }
+          } else {
+            filter.operators = [];
+            filter.comparison = "";
+          }
+        }
+      },
+      add: function() {
+        var filter = {
+          key: this.fields ? this.fields[0].name : "Id",
+          value: "",
+          comparison: ""
+        };
+        this.selectField(filter);
+        this.values.push(filter);
+      },
+      remove: function(i) {
+        this.values.splice(i, 1);
+      }
+    },
+    watch: {
+      fields: function(val) {
+        var self = this;
+        this.values = [];
+        if (val && val.length) {
+          if (
+            this.value !== undefined &&
+            this.value !== "" &&
+            this.value !== "[{}]"
+          ) {
+            var values = JSON.parse(this.value);
+            if (Array.isArray(values) && values.length) {
+              values.forEach(function(item) {
+                var filter = {
+                  key: item.FieldName,
+                  value: item.FieldValue,
+                  comparison: item.Comparer
+                };
+                self.selectField(filter);
+                self.values.push(filter);
+              });
+            }
+          }
+        }
+      },
+      values: {
+        handler: function(val) {
+          this.debounceChange();
+        },
+        deep: true
       }
     }
   });
 
   Vue.component("control-dictionary", {
-    inheritAttrs: false,
     template: Kooboo.getTemplate(
       "/_Admin/Scripts/vue-components/extensionEditor/dictionary.html"
     ),
-    props: ["name", "value"],
-    inheritAttrs: false,
+    extends: controlBase,
     data: function() {
       return {
         values: []
@@ -83,8 +182,7 @@
     template: Kooboo.getTemplate(
       "/_Admin/Scripts/vue-components/extensionEditor/collection.html"
     ),
-    inheritAttrs: false,
-    props: ["name", "value"],
+    extends: controlBase,
     data: function() {
       return {
         values: []
@@ -117,145 +215,6 @@
       }
     },
     watch: {
-      values: {
-        handler: function(val) {
-          this.$emit("change", {
-            name: this.name,
-            value: this.values
-          });
-        },
-        deep: true
-      }
-    }
-  });
-
-  Vue.component("control-checkbox", {
-    template: Kooboo.getTemplate(
-      "/_Admin/Scripts/vue-components/extensionEditor/checkbox.html"
-    ),
-    inheritAttrs: false,
-    props: ["name", "value"],
-    data: function() {
-      return {
-        valueBool: false
-      };
-    },
-    mounted: function() {
-      this.valueBool = this.value === "True" || this.value === "true";
-    },
-    watch: {
-      valueBool: function(val) {
-        this.$emit("change", {
-          name: this.name,
-          value: val
-        });
-      }
-    }
-  });
-
-  Vue.component("control-order", {
-    template: Kooboo.getTemplate(
-      "/_Admin/Scripts/vue-components/extensionEditor/orderBy.html"
-    ),
-    props: ["name", "value", "fields"],
-    methods: {
-      change: function(value) {
-        this.$emit("change", {
-          name: this.name,
-          value: value
-        });
-      }
-    }
-  });
-
-  function Filter(ob, fields) {
-    var choosedOperator;
-    if (ob.key) {
-      choosedOperator = fields.filter(function(item) {
-        return item.name === ob.key;
-      })[0];
-    }
-    if (choosedOperator !== undefined) {
-      ob.operators = choosedOperator.operators;
-    } else {
-      ob.operators = [];
-    }
-    return ob;
-  }
-
-  Vue.component("control-filter", {
-    template: Kooboo.getTemplate(
-      "/_Admin/Scripts/vue-components/extensionEditor/filter.html"
-    ),
-    props: ["name", "value", "fields"],
-    data: function() {
-      return {
-        values: []
-      };
-    },
-    methods: {
-      chooseField: function(filter) {
-        if (this.fields) {
-          var choosedOperator = this.fields.filter(function(item) {
-            return item.name === filter.key;
-          })[0];
-          filter.operators = choosedOperator ? choosedOperator.operators : [];
-          if (
-            !filter.comparison ||
-            filter.operators.indexOf(filter.comparison) === -1
-          ) {
-            filter.comparison = filter.operators[0];
-          }
-        }
-      },
-      add: function() {
-        var newFilter = Filter(
-          {
-            key: this.fields ? this.fields[0].name : "Id",
-            value: "",
-            comparison: ""
-          },
-          this.fields
-        );
-        this.chooseField(newFilter);
-        this.values.push(newFilter);
-      },
-      remove: function(i) {
-        this.values.splice(i, 1);
-      }
-    },
-    watch: {
-      fields: {
-        handler: function(val) {
-          if (val) {
-            var self = this;
-            this.values = [];
-            if (
-              this.value !== undefined &&
-              this.value !== "" &&
-              this.value !== "[{}]"
-            ) {
-              if (
-                JSON.parse(this.value) instanceof Array &&
-                JSON.parse(this.value).length > 0
-              ) {
-                JSON.parse(self.value).forEach(function(item) {
-                  console.log(item);
-                  var filter = Filter(
-                    {
-                      key: item.FieldName,
-                      value: item.FieldValue,
-                      comparison: item.Comparer
-                    },
-                    self.fields
-                  );
-                  self.values.push(filter);
-                });
-              }
-            }
-          }
-        }
-      },
       values: {
         handler: function(val) {
           this.$emit("change", {
