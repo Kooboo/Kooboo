c7cdb0d971410e318219eda3c21a73558bff6789
wenjie <wenjie@kooboo.cn>
2019-12-16 15:13:24 +0800

    fixed: repeat-self bugs

--- Kooboo.Web/_Admin/Scripts/viewEditor/widgets/Position.js
+++ Kooboo.Web/_Admin/Scripts/viewEditor/widgets/Position.js
@@ -73,13 +73,14 @@
                         case 'data':
                             var repeatSelf = !!$(elem).attr('k-repeat-self');
                             if(repeatSelf) {
-                                dataContext = DataContext.create(elem, contextStack, true);
+                                dataContext = DataContext.get(elem);
                                 var _key = Object.keys(dataContext.value())[0];
+                                var dataId = dataContext.value(true)[_key].dataId;
                                 list.push({
                                     elem: elem,
                                     bindingType: key,
                                     text: val.text,
-                                    dataSourceId: dataContext.value(true)[_key].dataId
+                                    dataSourceId: dataId
                                 });
                             } else {
                                 dataContext = DataContext.create(elem, contextStack, false);
@@ -92,26 +93,50 @@
                             }
                             break;
                         case 'attribute':
-                            dataContext = DataContext.createByAttribute(elem, contextStack);
                             var ids = [];
-                            val.text.split(";").forEach(function(attr) {
-                                if (attr) {
-                                    var value = attr.split(" ")[1],
-                                        sources = value.match(/[^\{\}]+(?=\})/g);
-                                    if (sources && sources.length) {
-                                        sources.forEach(function(src) {
-                                            var find = dataContext.lookup(src);
-                                            find && ids.indexOf(find.dataId) == -1 && ids.push(find.dataId);
-                                        })
+                            var repeatSelf = !!$(elem).attr('k-repeat-self');
+                            if(repeatSelf) {
+                                dataContext = DataContext.get(elem);
+                                val.text.split(";").forEach(function(attr) {
+                                    if (attr) {
+                                        var value = attr.split(" ")[1],
+                                            sources = value.match(/[^\{\}]+(?=\})/g);
+                                        if (sources && sources.length) {
+                                            sources.forEach(function(src) {
+                                                var _key = src.split('.')[0];
+                                                var dataId = dataContext.value(true)[_key].dataId;
+                                                ids.indexOf(dataId) == -1 && ids.push(dataId);
+                                            });
+                                        }
                                     }
-                                }
-                            })
-                            list.push({
-                                elem: elem,
-                                bindingType: key,
-                                text: val.text,
-                                ids: ids
-                            });
+                                });
+                                list.push({
+                                    elem: elem,
+                                    bindingType: key,
+                                    text: val.text,
+                                    ids: ids
+                                });
+                            } else {
+                                dataContext = DataContext.createByAttribute(elem, contextStack);
+                                val.text.split(";").forEach(function(attr) {
+                                    if (attr) {
+                                        var value = attr.split(" ")[1],
+                                            sources = value.match(/[^\{\}]+(?=\})/g);
+                                        if (sources && sources.length) {
+                                            sources.forEach(function(src) {
+                                                var find = dataContext.lookup(src);
+                                                find && ids.indexOf(find.dataId) == -1 && ids.push(find.dataId);
+                                            });
+                                        }
+                                    }
+                                });
+                                list.push({
+                                    elem: elem,
+                                    bindingType: key,
+                                    text: val.text,
+                                    ids: ids
+                                });
+                            }
                             break;
                         case 'repeat':
                             dataContext = DataContext.create(elem, contextStack, true);
@@ -123,6 +148,7 @@
 
                                 if (repeatSelf) {
                                     _key = Object.keys(dataContext.value())[0];
+                                    $(elem).data('kb-datacontext', dataContext);
                                 } else {
                                     _key = Object.keys(DataContext.get(elem.parentNode).value())[0];
                                 }
