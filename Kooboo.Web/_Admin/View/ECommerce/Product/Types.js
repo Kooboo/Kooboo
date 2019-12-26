c94f04c459c0003a78a7e16713b3961f85d70ae7
wenjie <wenjie@kooboo.cn>
2019-12-26 14:23:51 +0800

    vue: product types

--- Kooboo.Web/_Admin/View/ECommerce/Product/Types.js
+++ Kooboo.Web/_Admin/View/ECommerce/Product/Types.js
@@ -1,47 +1,63 @@
 $(function() {
-
-    var typesModel = function() {
-        var self = this;
-
-        this.newTypeUrl = Kooboo.Route.Product.Type.DetailPage;
-
-        getList();
-
-        function getList() {
-            Kooboo.ProductType.getList().then(function(res) {
-                if (res.success) {
-                    var docs = res.model.map(function(item) {
-                        return {
-                            id: item.id,
-                            name: item.name,
-                            edit: {
-                                text: Kooboo.text.common.edit,
-                                url: Kooboo.Route.Get(Kooboo.Route.Product.Type.DetailPage, {
-                                    id: item.id
-                                })
-                            }
-                        }
-                    })
-
-                    self.tableData({
-                        docs: docs,
-                        columns: [{
-                            displayName: Kooboo.text.common.name,
-                            fieldName: 'name',
-                            type: 'text'
-                        }],
-                        tableActions: [{
-                            fieldName: 'edit',
-                            type: 'link-btn'
-                        }],
-                        kbType: Kooboo.ProductType.name
-                    })
-                }
-            })
+  var self;
+  new Vue({
+    el: "#app",
+    data: function() {
+      self = this;
+      return {
+        breads: [
+          {
+            name: "SITES"
+          },
+          {
+            name: "DASHBOARD"
+          },
+          {
+            name: Kooboo.text.common.ProductTypes
+          }
+        ],
+        newTypeUrl: Kooboo.Route.Product.Type.DetailPage,
+        tableData: [],
+        selected: []
+      };
+    },
+    mounted: function() {
+      Kooboo.ProductType.getList().then(function(res) {
+        if (res.success) {
+          self.tableData = res.model.map(function(item) {
+            return {
+              id: item.id,
+              name: item.name,
+              edit: {
+                text: Kooboo.text.common.edit,
+                url: Kooboo.Route.Get(Kooboo.Route.Product.Type.DetailPage, {
+                  id: item.id
+                })
+              }
+            };
+          });
         }
+      });
+    },
+    methods: {
+      onDelete: function() {
+        if (confirm(Kooboo.text.confirm.deleteItems)) {
+          var ids = self.selected.map(function(row) {
+            return row.id;
+          });
+          Kooboo.ProductType.Deletes({
+            ids: JSON.stringify(ids)
+          }).then(function(res) {
+            if (res.success) {
+              self.tableData = _.filter(self.tableData, function(row) {
+                return ids.indexOf(row.id) === -1;
+              });
+              self.selected = [];
+              window.info.show(Kooboo.text.info.delete.success, true);
+            }
+          });
+        }
+      }
     }
-
-    typesModel.prototype = new Kooboo.tableModel(Kooboo.ProductType.name);
-    var vm = new typesModel();
-    ko.applyBindings(vm, document.getElementById('main'));
-})
+  });
+});
