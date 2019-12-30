7f0a1a2c6e0cfcfdeb486a9c725574c2b57c6523
wenjie <wenjie@kooboo.cn>
2019-12-30 10:54:39 +0800

    merge

--- Kooboo.Web/Api/Implementation/Ecommerce/ProductType.cs
+++ Kooboo.Web/Api/Implementation/Ecommerce/ProductType.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Kooboo.Data.Definition;
 
 namespace Kooboo.Web.Api.Implementation.Ecommerce
 {
