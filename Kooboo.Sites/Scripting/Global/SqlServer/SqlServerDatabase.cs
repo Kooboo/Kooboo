f8224694babcf06a15b0e9fea62363e5b3ec0aa0
huanent <huanent@DESKTOP-GEVT7VG>
2020-01-22 14:05:28 +0800

    add sqlserver

---
+++ Kooboo.Sites/Scripting/Global/SqlServer/SqlServerDatabase.cs
@@ -0,0 +1,16 @@
+﻿using Kooboo.Sites.Scripting.Global.RelationalDatabase;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Kooboo.Sites.Scripting.Global.Mysql
+{
+    public class SqlServerDatabase : RelationalDatabase<SqlServerExecuter, SqlServerSchema, MySqlConnection>
+    {
+        public SqlServerDatabase(string connectionString) : base(connectionString)
+        {
+        }
+    }
+}
---
+++ Kooboo.Sites/Scripting/Global/SqlServer/SqlServerSchema.cs
@@ -0,0 +1,44 @@
+﻿using Kooboo.Sites.Scripting.Global.RelationalDatabase;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Kooboo.Sites.Scripting.Global.Mysql
+{
+    public class SqlServerSchema : RelationalSchema
+    {
+        public SqlServerSchema(IEnumerable<Item> items) : base(items)
+        {
+        }
+
+        public SqlServerSchema(IDictionary<string, object> keyValuePairs) : base(keyValuePairs)
+        {
+        }
+
+        static readonly string[] _textType = new[] { "CHAR", "VARCHAR", "TINYBLOB", "TINYTEXT", "BLOB", "TEXT", "MEDIUMBLOB", "MEDIUMTEXT", "LONGBLOB", "LONGTEXT"};
+        static readonly string[] _dateTime = new[] { "DATE", "TIME", "YEAR", "DATETIME" };
+        static readonly string[] _double = new[] { "FLOAT", "DOUBLE", "DECIMAL", "BIGINT", "INTEGER", "INT", "MEDIUMINT", "SMALLINT", "YEAR","TINYINT" };
+
+        internal override string ConventType(Type type)
+        {
+            if (type == typeof(string)) return "TEXT";
+            if (type == typeof(DateTime)) return "DATETIME";
+            if (type == typeof(double) || type == typeof(int) || type == typeof(float) || type == typeof(decimal)) return "DOUBLE";
+            if (type == null) return "NULL";
+            if (type == typeof(bool)) return "TINYINT";
+            throw new NotSupportedException();
+        }
+
+        internal override bool CompatibleType(string dbType, string jsType)
+        {
+            if (jsType == "NULL") return true;
+            if (jsType == "TEXT" && _textType.Any(a => dbType.ToUpper().StartsWith(a))) return true;
+            if (jsType == "DATETIME" && _dateTime.Any(a => dbType.ToUpper().StartsWith(a))) return true;
+            if (jsType == "DOUBLE" && _double.Any(a => dbType.ToUpper().StartsWith(a))) return true;
+            if (jsType == "TINYINT" && _double.Any(a => dbType.ToUpper().StartsWith(a))) return true;
+            return false;
+        }
+    }
+}
