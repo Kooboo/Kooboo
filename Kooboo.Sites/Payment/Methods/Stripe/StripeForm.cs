8b0e8e2fbf65c0052d0b0cb54e302ca08e7dad8f
Yukun <yukun@kooboo.cn>
2020-02-24 18:03:38 +0800

    Setup Stripe

---
+++ Kooboo.Sites/Payment/Methods/Stripe/StripeForm.cs
@@ -0,0 +1,34 @@
+﻿using Kooboo.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kooboo.Sites.Payment.Methods.Stripe
+{
+    public class StripeForm : IPaymentMethod<StripeFormSetting>
+    {
+        public StripeFormSetting Setting { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string Name => throw new NotImplementedException();
+
+        public string DisplayName => throw new NotImplementedException();
+
+        public string Icon => throw new NotImplementedException();
+
+        public string IconType => throw new NotImplementedException();
+
+        public List<string> supportedCurrency => throw new NotImplementedException();
+
+        public RenderContext Context { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public IPaymentResponse Charge(PaymentRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PaymentStatusResponse checkStatus(PaymentRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Kooboo.Sites/Payment/Methods/Stripe/StripeFormSetting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kooboo.Sites.Payment.Methods.Stripe
+{
+    public class StripeFormSetting : IPaymentSetting
+    {
+        private const string name = "StripPayment";
+
+        public string Name => name;
+     }
+}
