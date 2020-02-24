f33f3a8f28c6ee0b7a0093038a58303bfae76933
李凤焕 <huanent@icloud.com>
2020-02-24 13:48:03 +0800

    changed tests

--- Kooboo.Web/kooboo-web-editor/test/components/floatMenu/items/editRepeatLink.test.ts
+++ Kooboo.Web/kooboo-web-editor/test/components/floatMenu/items/editRepeatLink.test.ts
@@ -27,13 +27,11 @@ describe("editRepeatLink", () => {
 
     // 不能是body元素
     context.lastSelectedDomEventArgs = new SelectedDomEventArgs(document.body as HTMLElement);
-    let comments = KoobooComment.getComments(document.body);
-    elementObject.update(comments);
+    elementObject.update();
     expect(elementObject.el.style.display).equal("none");
 
     context.lastSelectedDomEventArgs = new SelectedDomEventArgs(document.body.children[0].children[0].children[0] as HTMLElement);
-    comments = KoobooComment.getComments(document.body.children[0].children[0].children[0]);
-    elementObject.update(comments);
+    elementObject.update();
     expect(elementObject.el.style.display).equal("block");
   });
 
@@ -57,7 +55,7 @@ describe("editRepeatLink", () => {
     expect(elementObject.el.style.display).equal("");
 
     let comments = KoobooComment.getComments(document.body.children[0].children[0].children[0]);
-    elementObject.update(comments);
+    elementObject.update();
     expect(elementObject.el.style.display).equal("none");
   });
 });
