6b20d6d66c0abf4af8377890dcc3bbb15c8a3142
tenghui <1246623393@qq.com>
2020-02-26 16:16:08 +0800

    refactor code

--- Kooboo.Sites/Payment/Methods/Square/lib/PaymentsApi.cs
+++ Kooboo.Sites/Payment/Methods/Square/lib/PaymentsApi.cs
@@ -21,7 +21,7 @@ namespace Kooboo.Sites.Payment.Methods.Square.lib
             var body = new Models.PaymentRequest { SourceId = nonce, IdempotencyKey = uuid, AmountMoney = amount };
 
             return ApiClient.Create("Bearer", setting.AccessToken)
-                .PostAsync(queryUrl, JsonSerialize(body)).Result.Content;
+                .PostJsonAsync(queryUrl, body).Result.Content;
         }
 
         // https://developer.squareup.com/docs/checkout-api-overview
@@ -30,7 +30,7 @@ namespace Kooboo.Sites.Payment.Methods.Square.lib
             var queryUrl = setting.BaseURL + "/v2/locations/" + setting.LocationId + "/checkouts";
 
             return ApiClient.Create("Bearer", setting.AccessToken)
-                .PostAsync(queryUrl, JsonSerialize(request)).Result.Content;
+                .PostJsonAsync(queryUrl, request).Result.Content;
         }
 
         // https://developer.squareup.com/reference/square/orders-api/batch-retrieve-orders#type-orderstate
@@ -39,28 +39,7 @@ namespace Kooboo.Sites.Payment.Methods.Square.lib
             var queryUrl = setting.BaseURL + "/v2/locations/" + setting.LocationId + "/orders/batch-retrieve";
 
             return ApiClient.Create("Bearer", setting.AccessToken)
-                .PostAsync(queryUrl, JsonSerialize(orderRequest)).Result.Content;
-        }
-
-        public static string JsonSerialize(object obj, JsonConverter converter = null)
-        {
-            if (null == obj)
-            {
-                return null;
-            }
-
-            var settings = new JsonSerializerSettings();
-
-            if (converter == null)
-            {
-                settings.Converters.Add(new IsoDateTimeConverter());
-            }
-            else
-            {
-                settings.Converters.Add(converter);
-            }
-
-            return JsonConvert.SerializeObject(obj, Formatting.None, settings);
+                .PostJsonAsync(queryUrl, orderRequest).Result.Content;
         }
     }
 }
