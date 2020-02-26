4c04a1f7d8322ebe9ff1869938f61c9f0fc55ac3
yukun-kooboo <yukun@kooboo.cn>
2020-02-26 17:20:51 +0800

    Using unify HTTP client

--- Kooboo.Sites/Payment/Methods/Stripe/lib/StripeUtility.cs
+++ Kooboo.Sites/Payment/Methods/Stripe/lib/StripeUtility.cs
@@ -1,7 +1,5 @@
 ﻿using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -41,15 +39,13 @@ namespace Kooboo.Sites.Payment.Methods.Stripe.lib
 
         public static async Task<string> CreateSession(SessionCreateOptions options, string Secretkey)
         {
-            using (var httpClient = new HttpClient())
-            {
-                httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + Secretkey);
-
-                var result = await httpClient.PostAsync("https://api.stripe.com/v1/checkout/sessions", new StringContent(StripeUtility.SessionDataToContentString(options), Encoding.UTF8, "application/x-www-form-urlencoded"));
-                var response = await result.Content.ReadAsStringAsync();
-                JObject json = JObject.Parse(response);
-                return json.Value<string>("id");
-            }
+            var client = ApiClient.Create();
+            var headers = new Dictionary<string, string>();
+            headers.Add("Authorization", "Bearer " + Secretkey);
+            var result = await client.PostAsync("https://api.stripe.com/v1/checkout/sessions", SessionDataToContentString(options), "application/x-www-form-urlencoded", headers);
+            var response =  result.Content;
+            JObject json = JObject.Parse(response);
+            return json.Value<string>("id");
         }
     }
 }
