2f16812cd23a9ce7859ece0d5ec1eba5c5fcd0d8
yukun-kooboo <yukun@kooboo.cn>
2020-02-27 11:31:51 +0800

    Adjust component description

--- Kooboo.Sites/Payment/Methods/Stripe/StripeFormSetting.cs
+++ Kooboo.Sites/Payment/Methods/Stripe/StripeFormSetting.cs
@@ -6,7 +6,7 @@ namespace Kooboo.Sites.Payment.Methods.Stripe
 {
     public class StripeFormSetting : IPaymentSetting
     {
-        private const string name = "StripPayment";
+        private const string name = "StripePayment";
 
         public string Name => name;
 
