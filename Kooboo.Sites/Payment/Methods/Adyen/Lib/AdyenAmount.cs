1320352c63913cfc36f799af9ae4bb456e854ade
maolin-kooboo <maolin@kooboo.cn>
2020-02-28 14:02:14 +0800

    add adyen payment method

---
+++ Kooboo.Sites/Payment/Methods/Adyen/Lib/AdyenAmount.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Kooboo.Sites.Payment.Methods.Adyen.Lib
+{
+    public class AdyenAmount
+    {
+        [JsonProperty("value")]
+        public long Value { get; set; }
+
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+    }
+}
---
+++ Kooboo.Sites/Payment/Methods/Adyen/Lib/ErrorResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Kooboo.Sites.Payment.Methods.Adyen.Lib
+{
+    public class ErrorResponse
+    {
+        [JsonProperty("status")]
+        public long Status { get; set; }
+
+        [JsonProperty("errorCode")]
+        public string ErrorCode { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("errorType")]
+        public string ErrorType { get; set; }
+    }
+}
