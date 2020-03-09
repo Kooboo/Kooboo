7fabfa37fe9fbbbca4a6133b50d24d6bb97ca2c5
李凤焕 <huanent@icloud.com>
2020-03-09 19:07:37 +0800

    move auto version to entry assembly

--- Kooboo.App.Standard/Properties/AssemblyInfo.cs
+++ Kooboo.App.Standard/Properties/AssemblyInfo.cs
@@ -1,28 +1,37 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Kooboo.DataAccess")]
+[assembly: AssemblyTitle("Kooboo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Kooboo.DataAccess")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("Kooboo.App")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("Kooboo.Data.Test")]
-[assembly: InternalsVisibleTo("Kooboo.Sites.Test")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("919321b3-c964-4726-a0eb-b910a1de3a83")]
+[assembly: Guid("390D0AB6-574A-4E59-97A6-1060A340CACA")]
+
+//In order to begin building localizable applications, set 
+//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+//inside a <PropertyGroup>.  For example, if you are using US english
+//in your source files, set the <UICulture> to en-US.  Then uncomment
+//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+//the line below to match the UICulture setting in the project file.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,8 +43,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: InternalsVisibleTo("Kooboo.Account.Tests")]
-[assembly: InternalsVisibleTo("Kooboo.Mail.Test")]
-[assembly: InternalsVisibleTo("Kooboo.Sites.Test2")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
