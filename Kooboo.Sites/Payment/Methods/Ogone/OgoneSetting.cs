e035a9f25b60f8a99f60bbcb648ee898285e2cfe
Linweimin-kooboo <weimin-kooboo@outlook.com>
2020-03-13 17:52:32 +0800

    ogone

---
+++ Kooboo.Sites/Payment/Methods/Ogone/OgoneSetting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kooboo.Sites.Payment.Methods.Ogone
+{
+    public class OgoneSetting : IPaymentSetting
+    {
+        public string Name => "OgonePay";
+
+        public bool UseSandBox { get; set; }
+
+        public string MerchantId { get; set; }
+
+        public string ApiKeyId { get; set; }
+
+        public string SecretApiKey { get; set; }
+
+        public string ServerURL => UseSandBox ? "https://eu.sandbox.api-ingenico.com" : "https://world.api-ingenico.com";
+
+        public string RedirectURL { get; set; }
+
+        public string BaseUrl()
+        {
+            return "https://payment";
+        }
+    }
+}
