a7930a51c9496f105cc6de944c258d35c2af81bb
李凤焕 <huanent@icloud.com>
2020-06-24 22:54:38 +0800

    temp

---
+++ Kooboo.DaemonUI/MainForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Kooboo.DaemonUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusTxt = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("startBtn.BackgroundImage")));
+            this.startBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.startBtn.Location = new System.Drawing.Point(12, 12);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Padding = new System.Windows.Forms.Padding(5);
+            this.startBtn.Size = new System.Drawing.Size(60, 40);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.Start);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("stopBtn.BackgroundImage")));
+            this.stopBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.stopBtn.Location = new System.Drawing.Point(78, 12);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Padding = new System.Windows.Forms.Padding(5);
+            this.stopBtn.Size = new System.Drawing.Size(60, 40);
+            this.stopBtn.TabIndex = 1;
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.Stop);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(503, 259);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "日志";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(497, 235);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(153, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 40);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "状态：";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // statusTxt
+            // 
+            this.statusTxt.Location = new System.Drawing.Point(230, 12);
+            this.statusTxt.Name = "statusTxt";
+            this.statusTxt.Size = new System.Drawing.Size(282, 40);
+            this.statusTxt.TabIndex = 4;
+            this.statusTxt.Text = "运行中";
+            this.statusTxt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 329);
+            this.Controls.Add(this.statusTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kooboo守护程序";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label statusTxt;
+    }
+}
---
+++ Kooboo.DaemonUI/MainForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kooboo.DaemonUI
+{
+    public partial class MainForm : Form
+    {
+        Process _process;
+
+        private bool Running
+        {
+            set
+            {
+                startBtn.Enabled = !value;
+                stopBtn.Enabled = value;
+                statusTxt.Text = value ? "运行中.." : "已停止";
+            }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Running = false;
+
+        }
+
+        private void Start(object sender, EventArgs e)
+        {
+            // Task
+            StartApp();
+        }
+
+        private void _process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            AddMsg(e.Data);
+        }
+
+        private void Stop(object sender, EventArgs e)
+        {
+            if (_process != null && !_process.HasExited)
+            {
+                AddMsg("正在停止");
+                _process.Kill();
+                Running = false;
+                AddMsg("停止成功");
+            }
+
+        }
+
+        private void AddMsg(string msg)
+        {
+            Invoke(new Action(() =>
+            {
+                StringBuilder sb = new StringBuilder(textBox1.Text);
+                textBox1.Text = sb.AppendLine(msg).ToString();
+                textBox1.SelectionStart = this.textBox1.Text.Length;
+                textBox1.ScrollToCaret();
+            }));
+        }
+
+        private void StartApp()
+        {
+            _process = new Process();
+            _process.StartInfo.FileName = "dotnet";
+            _process.StartInfo.Arguments = "Kooboo.App.dll";
+            _process.StartInfo.UseShellExecute = false;
+            _process.StartInfo.RedirectStandardInput = true;
+            _process.StartInfo.RedirectStandardOutput = true;
+            _process.StartInfo.CreateNoWindow = true;
+            _process.OutputDataReceived += _process_OutputDataReceived;
+            _process.Start();
+            _process.BeginOutputReadLine();
+            Running = true;
+        }
+    }
+}
