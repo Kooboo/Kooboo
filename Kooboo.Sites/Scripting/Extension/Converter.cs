e9cea6741c684cea4b1cdc71851e76fbf4409bb3
zguoqi <guoqi@kooboo.com>
2020-08-09 15:42:54 +0800

    office converter with PDF support

--- Kooboo.Sites/Scripting/Extension/Converter.cs
+++ Kooboo.Sites/Scripting/Extension/Converter.cs
@@ -124,30 +124,12 @@ namespace Kooboo.Sites.Scripting.Extension
                         continue;
                     }
 
-                    string RelativeUrl = Kooboo.Lib.Helper.UrlHelper.RelativePath(item.Value);
-
-
-                    if (string.IsNullOrWhiteSpace(RelativeUrl))
+                    if (string.IsNullOrWhiteSpace(item.Value))
                     {
-                        continue; 
+                        continue;
                     }
 
-                    // add converter folder. 
-                   /// RelativeUrl = Lib.Helper.UrlHelper.Combine("officeconverter", RelativeUrl); 
-
-                    if (RelativeUrl.StartsWith("/") || RelativeUrl.Contains("/"))
-                    {
-                        RelativeUrl = "/officeconverter" + RelativeUrl; 
-                    }
-                    else if (RelativeUrl.StartsWith("\\") || RelativeUrl.Contains("\\"))
-                    {
-                        RelativeUrl = "\\officeconverter" + RelativeUrl; 
-                    }
-                    else
-                    {
-                        RelativeUrl = "/officeconverter/" + RelativeUrl;
-                    }
-                     
+                    string RelativeUrl = AppendPath(item.Value);
 
                     if (item.Value != RelativeUrl)
                     {
@@ -158,21 +140,81 @@ namespace Kooboo.Sites.Scripting.Extension
                         {
                             StartIndex = item.Key.location.openTokenStartIndex,
                             EndIndex = item.Key.location.openTokenEndIndex,
-                            NewValue = newstring 
+                            NewValue = newstring
                         });
                     }
-                     
+
                 }
             }
 
 
-            var list = dom.getElementsByTagName("link"); 
-           
+            // for external css link. 
+            var list = dom.getElementsByTagName("link");
 
+            foreach (var item in list.item)
+            {
+                //  < link rel = "stylesheet" href = " " >
+                var rel = item.getAttribute("rel"); 
+                if (rel !=null && rel.ToLower() == "stylesheet")
+                {
+                    var href = item.getAttribute("href"); 
+                    if (string.IsNullOrWhiteSpace(href))
+                    {
+                        continue; 
+                    }
+                    else
+                    { 
+                        string RelativeUrl = AppendPath(href); 
+                        if (href != RelativeUrl)
+                        {
+                            string oldstring = Kooboo.Sites.Service.DomService.GetOpenTag(item);
+                            string newstring = oldstring.Replace(href, RelativeUrl);
+
+                            updates.Add(new SourceUpdate()
+                            {
+                                StartIndex = item.location.openTokenStartIndex,
+                                EndIndex = item.location.openTokenEndIndex,
+                                NewValue = newstring
+                            });
+                        }  
+                    }
+
+                }
+            }
             
 
-            var embeds = dom.getElementsByTagName("embed"); 
+            var embeds = dom.getElementsByTagName("embed");
+            //<embed src = "UpcE1I9h3kCZ1P2oBSKRpw_files/img_01.svg" type = "image/svg+xml" class="stl_03" />
 
+            foreach (var item in embeds.item)
+            {
+                var type = item.getAttribute("type");
+                if (type !=null & type.ToLower().Contains("image"))
+                {
+                    var href = item.getAttribute("src");
+                    if (string.IsNullOrWhiteSpace(href))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        string RelativeUrl = AppendPath(href);
+                        if (href != RelativeUrl)
+                        {
+                            string oldstring = Kooboo.Sites.Service.DomService.GetOpenTag(item);
+                            string newstring = oldstring.Replace(href, RelativeUrl);
+
+                            updates.Add(new SourceUpdate()
+                            {
+                                StartIndex = item.location.openTokenStartIndex,
+                                EndIndex = item.location.openTokenEndIndex,
+                                NewValue = newstring
+                            });
+                        }
+                    } 
+                }
+          
+            }
 
 
              if (updates.Any())
@@ -180,6 +222,26 @@ namespace Kooboo.Sites.Scripting.Extension
                 return Kooboo.Sites.Service.DomService.UpdateSource(input, updates);  
             }
             return input;  
+        }
+
+        private static string AppendPath(string urlpath)
+        {
+            string RelativeUrl = Kooboo.Lib.Helper.UrlHelper.RelativePath(urlpath);
+
+            if (RelativeUrl.StartsWith("/") || RelativeUrl.Contains("/"))
+            {
+                RelativeUrl = "/officeconverter" + RelativeUrl;
+            }
+            else if (RelativeUrl.StartsWith("\\") || RelativeUrl.Contains("\\"))
+            {
+                RelativeUrl = "\\officeconverter" + RelativeUrl;
+            }
+            else
+            {
+                RelativeUrl = "/officeconverter/" + RelativeUrl;
+            }
+
+            return RelativeUrl;
         }
 
         public string OfficeToCleanHTML(byte[] officebytes, string filename)
