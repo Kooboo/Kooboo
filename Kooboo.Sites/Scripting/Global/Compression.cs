4f7449b04e8c95bd7ef8d33cb3d1242afe39bbc5
zguoqi <guoqi@kooboo.com>
2020-11-08 19:41:02 +0800

    site analyzer link group

---
+++ Kooboo.Sites/Scripting/Global/Compression.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO.Compression;
+using Kooboo.Data.Context;
+
+namespace Kooboo.Sites.Scripting.Global
+{
+    public class Compression
+    {
+
+        private RenderContext context { get; set; }
+
+        public Compression(RenderContext context)
+        {
+            this.context = context;
+        }
+
+        public object Create()
+        {
+            return null; 
+        } 
+
+    }
+}
