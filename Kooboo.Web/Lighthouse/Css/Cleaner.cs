a9b6a4eb5bf6f151006ead2b6a64cc02247ab750
zguoqi <guoqi@kooboo.com>
2021-02-08 15:25:10 +0800

    light house

---
+++ Kooboo.Web/Lighthouse/Css/Cleaner.cs
@@ -0,0 +1,13 @@
+﻿using Kooboo.Sites.Repository;
+using System.Collections.Generic;
+
+namespace Kooboo.Web.Lighthouse.Css
+{
+    public static class Cleaner
+    { 
+      static  List<Kooboo.Sites.Models.CmsCssRule> QueryUnused(SiteDb sitedb)
+        { 
+            return null; 
+        } 
+    }
+}
